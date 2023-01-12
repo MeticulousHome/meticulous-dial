25aa657d5c3ad5313b81994192f4a2a6405c266b
salatielosorno <salatiel.linares@gmail.com>
2023-01-12 17:04:30 -0600

    feat(src/preload): enable env values

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import * as ReactDOM from 'react-dom/client';
 import { Barometer } from './components/Barometer/Barometer';
 
 const App = (): JSX.Element => {
+  //@ts-ignore
+  console.info(window.meticulous_envs.SERVER_URL());
+
   const sensors = { p: '0', t: '0', w: '0', f: '0' };
   const time = '0';
   const name = 'IDLE';
--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,8 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge } from 'electron';
+
+contextBridge.exposeInMainWorld('meticulous_envs', {
+  SERVER_URL: () => process.env.SERVER_URL
+  // we can also expose variables, not just functions
+});
