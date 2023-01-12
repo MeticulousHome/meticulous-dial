b143bf69d2ab5f11209c1d6dbd26d5bc7dd5157b
salatielosorno <salatiel.linares@gmail.com>
2023-01-12 17:02:58 -0600

    feat(src/components/barometer): calculate value from pressure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import * as ReactDOM from 'react-dom/client';
+
 import { Barometer } from './components/Barometer/Barometer';
 
 const App = (): JSX.Element => {
   const sensors = { p: '0', t: '0', w: '0', f: '0' };
   const time = '0';
   const name = 'IDLE';
-  return <Barometer sensors={sensors} name={name} time={time} />;
+
+  return <Barometer stats={{ sensors, time, name }} />;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
