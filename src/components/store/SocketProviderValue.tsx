27f4a0976b6eb7454295b78077ba0c5119b3e23f
salatielosorno <salatiel.linares@gmail.com>
2023-01-16 14:07:09 -0600

    feat(store): add socket manager
    add socket manager, types and listeners

---
+++ src/components/store/SocketProviderValue.tsx
@@ -0,0 +1,70 @@
+import { ThunkDispatch } from '@reduxjs/toolkit';
+import { useEffect } from 'react';
+// import { io } from "socket.io-client";
+import { StageType, ActionType } from '../../types/index';
+
+export interface SensorDataInterface {
+  name: StageType;
+  sensors: {
+    p: string; // Pressure - Bars
+    f: string; // Flow - ml/s
+    w: string; // Weight - grams
+    t: string; // Temperature - degrees celcius
+  };
+  time: string; // seconds
+}
+
+interface SocketProviderValueInterface {
+  sendAction: (name: ActionType) => void;
+  sendPreset: (name: string) => void;
+}
+
+//@ts-ignore
+//const socket: Socket | null = io(window.meticulous_envs.SERVER_URL());
+
+export const SocketProviderValue = (): SocketProviderValueInterface => {
+  useEffect(() => {
+    /* socket.on("connect", () => {
+            //dispatch
+        }); */
+    /* socket.on("disconnect", () => {
+            //
+        }); */
+    /* socket.on("status", (data: SensorDataInterface) => {
+            //dispatch
+        }); */
+  }, []);
+
+  const sendAction = (name: ActionType) => {
+    //socket.emit("action", name.toLowerCase());
+  };
+
+  const sendPreset = (name: string) => {
+    //socket.emit("preset", name.toLowerCase());
+  };
+
+  return { sendAction, sendPreset };
+};
+
+export const SetSocketKeyboardListeners = () => {
+  useEffect(() => {
+    window.addEventListener('keydown', (e) => {
+      switch (e.code) {
+        case 'ArrowLeft':
+          // dispatch Gesture
+          console.log('LEFT');
+          break;
+        case 'ArrowRight':
+          // dispatch Gesture
+          console.log('RIGHT');
+          break;
+        case 'Space':
+          // dispatch Gesture
+          console.log('SPACE');
+          break;
+        default:
+          break;
+      }
+    });
+  }, []);
+};
