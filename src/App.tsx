bf1a3be95621077ec9890f47b25d33e67de0a8ab
salatielosorno <salatiel.linares@gmail.com>
2023-01-16 20:18:05 -0600

    feat(store): add redux toolkit
    handle the state of gestures

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useRef } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
+import { store } from './components/store/store';
 import { Barometer } from './components/Barometer/Barometer';
 import { SockerManager } from './components/store/SockerManager';
+import { useAppSelector } from './components/store/hooks';
 
 const App = (): JSX.Element => {
   //@ts-ignore
@@ -20,4 +23,8 @@ const App = (): JSX.Element => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ src/components/store/features/gestures/gestures-slice.ts
@@ -0,0 +1,24 @@
+import { createSlice, Draft, PayloadAction } from '@reduxjs/toolkit';
+
+import { GestureType } from '../../../../types/index';
+
+interface GesturesState {
+  value: GestureType;
+}
+
+const initialState: GesturesState = {
+  value: ''
+};
+
+const gestureSlice = createSlice({
+  name: 'gesture',
+  initialState,
+  reducers: {
+    setGesture: (state: GesturesState, action: { payload: GestureType }) => {
+      state.value = action.payload;
+    }
+  }
+});
+
+export const { setGesture } = gestureSlice.actions;
+export default gestureSlice.reducer;
