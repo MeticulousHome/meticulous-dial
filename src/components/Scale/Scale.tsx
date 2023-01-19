68841689828ae0fa951c989c7957de73916a8550
unknown <juanjijo@gmail.com>
2023-01-19 13:09:18 -0600

    feat(scale): scale weight

---
+++ src/components/Scale/Scale.tsx
@@ -0,0 +1,20 @@
+import './scale.css';
+
+export function Scale(): JSX.Element {
+  return (
+    <div className="main-layout">
+      <div className="main-layout-content">
+        <div className="pressets-options-conainer">
+          <div className="title-main-2">scale</div>
+          <div className="scale-weight">
+            <div className="weight">
+              <span>00</span>
+              8.1
+            </div>
+            <div className="weight-data">g</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
