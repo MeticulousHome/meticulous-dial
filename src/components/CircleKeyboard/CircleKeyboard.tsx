d06ec99d19dc9650b001d4e8ad6a4f923144e472
Juan Antonio Rosales Fuentes <jrosales@Juans-MacBook-Pro.local>
2023-01-27 18:07:52 -0600

    feat(keybaord): add circle keyboard

--- src/components/CircleKeyboard/CircleKeyboard.tsx
+++ src/components/CircleKeyboard/CircleKeyboard.tsx
@@ -1,56 +1,16 @@
 import './circle-keyboard.css';
-import { useState } from 'react';
-const letters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S ',
-  'T',
-  ' ',
-  'U',
-  ' ',
-  'V',
-  'W',
-  'X',
-  'Y',
-  '<',
-  '_',
-  '^',
-  '|',
-  '#',
-  'Z',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '0'
-];
+import '../../assets/fonts/custom-space/css/fontello.css';
+import { useEffect, useState } from 'react';
+import { letters } from './Keys';
+import { useReduxSelector } from '../store/store';
 
 export function CircleKeyboard(): JSX.Element {
-  const [rotate, setRotate] = useState<number>(203.5);
+  const G_ROTATE = 7.24;
+  const [rotate, setRotate] = useState<number>(208);
   const [alphabet, setAlphabet] = useState<string[]>(letters);
   const [mainLetter, setMainLetter] = useState<string>('U');
   const [myIndex, setMyindex] = useState<number>(0);
+  const gesture = useReduxSelector((state) => state.gesture);
 
   const moveElements = (right: boolean) => {
     const newAlphabet = [...alphabet];
@@ -60,12 +20,13 @@ export function CircleKeyboard(): JSX.Element {
     const newLetter = alphabet[pm2];
     const p1 = i + 1;
     const m1 = i - 1;
+    // const [capsLockActive, setCapsLockActive] = useState<boolean>(false);
 
     if (!alphabet[pm2]) {
       return;
     }
 
-    setRotate(pmr + (!right ? +0.21 : -0.21));
+    setRotate(pmr);
 
     newAlphabet[i] = ' ';
     newAlphabet[pm2] = ' ';
@@ -78,9 +39,22 @@ export function CircleKeyboard(): JSX.Element {
     setMyindex(myIndex + 1);
   };
 
+  useEffect(() => {
+    if (gesture.value === 'left') {
+      moveElements(false);
+    }
+    if (gesture.value === 'right') {
+      moveElements(true);
+    }
+  }, [gesture]);
+
   return (
     <div className="circle-keyboard-container">
-      <div className="main-letter">{mainLetter}</div>
+      {mainLetter === '&#xe800;' ? (
+        <div className="main-letter-space">&#xe800;</div>
+      ) : (
+        <div className="main-letter">{mainLetter}</div>
+      )}
       <svg height="390" width="390">
         <svg viewBox="0 0 100 100">
           <g
@@ -88,14 +62,24 @@ export function CircleKeyboard(): JSX.Element {
             dominantBaseline="text-bottom"
             textAnchor="middle"
             textLength="120%"
+            className="transition-all"
           >
             {alphabet.map((letter: string, index: number) => {
               if (letter === mainLetter) {
                 return;
               }
+              if (letter === '&#xe800;') {
+                return (
+                  <g transform={`rotate(${index * G_ROTATE})`} key={index}>
+                    <text y={-44} textAnchor="-120%" className="letter-space">
+                      &#xe800;
+                    </text>
+                  </g>
+                );
+              }
               return (
-                <g transform={`rotate(${index * 7.45})`} key={index}>
-                  <text y={-44} className="letter">
+                <g transform={`rotate(${index * G_ROTATE})`} key={index}>
+                  <text y={-44} className="letter" textAnchor="-120%">
                     {letter}
                   </text>
                 </g>
@@ -104,25 +88,6 @@ export function CircleKeyboard(): JSX.Element {
           </g>
         </svg>
       </svg>
-      <div style={{ position: 'absolute', fill: 'rebeccapurple' }}>
-        <button
-          id="arrow-left"
-          onClick={() => {
-            moveElements(false);
-          }}
-        >
-          Left
-        </button>
-        <button id="space">Click</button>
-        <button
-          id="arrow-right"
-          onClick={() => {
-            moveElements(true);
-          }}
-        >
-          Right
-        </button>
-      </div>
     </div>
   );
 }
