cf36dc726d591930d5da75644d73f80336eaedd1
Phong Vu <caophong.w@gmail.com>
2023-02-06 16:47:55 +0700

    feat(slider): add slider for title, add keyboard interaction
    add keyboard interaction in presets, make main title into slider, and add animation for main title
    in preset mode

---
+++ src/components/MainTitle/index.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { useAppSelector } from '../store/hooks';
+
+const MainTitle = () => {
+  const { presets, screen } = useAppSelector((state) => state);
+  const [animationStyle, setAnimationStyle] = useState('');
+  const [swiper, setSwiper] = useState(null);
+
+  const slideTo = (index: number) => swiper.slideTo(index);
+
+  useEffect(() => {
+    if (presets.activePreset > -1) {
+      slideTo(presets.activePreset);
+    }
+  }, [presets.activePreset]);
+
+  return (
+    <Swiper
+      preventInteractionOnTransition={true}
+      slidesPerView={screen.value === 'pressets' ? 2 : 1}
+      centeredSlides={true}
+      allowTouchMove={false}
+      onSwiper={setSwiper}
+      onSlideChangeTransitionStart={() => {
+        setAnimationStyle('animation-bounce-left');
+      }}
+      onSlidePrevTransitionStart={() => {
+        setAnimationStyle('animation-bounce-right');
+      }}
+    >
+      {presets.value.map((preset, index) => (
+        <SwiperSlide key={`${index}-slide`}>
+          {({ isActive }) => (
+            <span className={isActive ? animationStyle : ''}>
+              {preset.name}
+            </span>
+          )}
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default MainTitle;
