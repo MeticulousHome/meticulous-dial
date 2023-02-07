355c7329b2c0d33374eef46d823d5b2fcae647ab
Phong Vu <caophong.w@gmail.com>
2023-02-08 01:40:42 +0700

    feat(preset setting): add slider for preset setting

--- src/components/PressetSettings/PressetSettings.tsx
+++ src/components/PressetSettings/PressetSettings.tsx
@@ -1,13 +1,100 @@
+import { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useAppSelector } from '../store/hooks';
+
 import './pressetSettings.css';
 
+const presetSettingOptionsMock = [
+  {
+    name: '',
+    id: -1
+  },
+  {
+    name: '',
+    id: -1
+  },
+  {
+    name: 'name: Filter 2.1',
+    id: 1
+  },
+  {
+    name: 'pressure',
+    id: 2
+  },
+  {
+    name: 'temperature: 86°c',
+    id: 3
+  },
+  {
+    name: 'stop weight',
+    id: 4
+  },
+  {
+    name: 'flow',
+    id: 5
+  },
+  {
+    name: 'pre-infusion',
+    id: 6
+  },
+  {
+    name: 'Ok',
+    id: 7
+  },
+  {
+    name: '',
+    id: -1
+  },
+  {
+    name: '',
+    id: -1
+  }
+];
+
 export function PressetSettings(): JSX.Element {
+  const [swiper, setSwiper] = useState(null);
+  const { presetSetting } = useAppSelector((state) => state);
+  const [animationStyle, setAnimationStyle] = useState('');
+  const [init, setInit] = useState(false);
+
+  // useEffect(() => {
+  //   if (swiper) {
+  //     swiper.slideTo(2);
+  //   }
+  // }, [swiper]);
+
+  useEffect(() => {
+    if (swiper) {
+      swiper.slideTo(presetSetting.activeSetting);
+    }
+  }, [presetSetting.activeSetting]);
+
+  useEffect(() => {
+    return () => {
+      setAnimationStyle('');
+    };
+  }, []);
+
   return (
-    <div className="main-layout">
-      <div className="main-layout-content">
-        <div className="pressets-options-conainer">
-          <div className="title-main-2">Filter 2.1</div>
-          <div className="presset-options">
-            <div className="presset-option-item">stop weight</div>
+    <div
+      style={{
+        position: 'relative',
+        height: '100%'
+      }}
+    >
+      <div
+        style={{
+          top: 0,
+          paddingTop: 60
+        }}
+        className="title-main-2"
+      >
+        Filter 2.1
+      </div>
+      <div className="blur-top"></div>
+      <div className="blur-bottom"></div>
+      <div className="presset-options">
+        {/* <div className="presset-option-item">stop weight</div>
             <div className="presset-option-item">flow</div>
             <div className="presset-option-item item-active">
               temperature: 86°c
@@ -19,12 +106,45 @@ export function PressetSettings(): JSX.Element {
             <div className="presset-option-item">temperature: 86°c</div>
             <div className="presset-option-item">presset</div>
             <div className="presset-option-item">style</div>
-            <div className="presset-option-item">save changes</div>
-          </div>
-        </div>
+            <div className="presset-option-item">save changes</div> */}
+        <Swiper
+          initialSlide={2}
+          slidesPerView={8}
+          onSwiper={setSwiper}
+          allowTouchMove={false}
+          direction="vertical"
+          autoHeight={false}
+          centeredSlides={true}
+          onSlideNextTransitionStart={() => {
+            if (!init) {
+              setInit(true);
+              return;
+            }
+            setAnimationStyle('animation-next');
+          }}
+          onSlidePrevTransitionStart={() => setAnimationStyle('animation-prev')}
+          onSlideChangeTransitionEnd={() => setAnimationStyle('')}
+        >
+          {presetSettingOptionsMock.map((option, index) => (
+            <SwiperSlide
+              className="presset-option-item"
+              key={`option-${index}`}
+            >
+              {({ isActive }) => (
+                <div
+                  className={`${animationStyle} ${
+                    isActive ? `item-active` : ''
+                  }`}
+                >
+                  {option.name}
+                </div>
+              )}
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
 
-      <div className="bottom-status">
+      <div className="bottom-status z2">
         <div className="flex">
           <div className="status-icon">
             <svg
