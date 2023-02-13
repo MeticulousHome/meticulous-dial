f7b305e16df78204ead4ebf7999e38c7b25659d2
salatielosorno <salatiel.linares@gmail.com>
2023-02-13 10:16:36 -0600

    ci(.github/workflows): allow values dinamically from secrets

--- forge.config.ts
+++ forge.config.ts
@@ -21,7 +21,7 @@ const config: ForgeConfig = {
     {
       name: '@electron-forge/publisher-s3',
       config: {
-        bucket: 'meticulous-dist',
+        bucket: process.env.S3_BUCKET,
         folder: process.env.DISTRIBUTION_FOLDER,
         public: false,
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
