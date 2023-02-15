cedcce6003272714da5068186f9883fa513d8423
Juan Antonio Rosales Fuentes <jrosales@Juans-MacBook-Pro.local>
2023-02-14 19:40:09 -0600

    feat(setting): update setting numerical

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,22 +1,4 @@
-const regex = /^-?[0-9]+$/;
-
-export const roundPrecision = (value: number, precision: number) => {
+export const roundPrecision = (value: number, precision: number): number => {
   const multiplier = Math.pow(10, precision || 0);
   return Math.round(value * multiplier) / multiplier;
-};
-
-export const formatStatValue = (value: string, padEnd: number): string => {
-  let fValue = 0.0;
-
-  try {
-    fValue = parseFloat(value);
-  } catch (e) {
-    return fValue.toString();
-  }
-
-  const finalNumber = roundPrecision(fValue, 1).toString();
-  if (regex.test(finalNumber)) {
-    return finalNumber + '.' + '0'.repeat(padEnd);
-  }
-  return finalNumber;
 };
