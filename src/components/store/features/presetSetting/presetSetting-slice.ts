02c7de53e32f2c74dd5105d452b2c7a054512dc3
Phong Vu <caophong.w@gmail.com>
2023-02-16 18:22:51 +0700

    feat(preset setting): add api call for setting, fix sliding issue

--- src/components/store/features/presetSetting/presetSetting-slice.ts
+++ src/components/store/features/presetSetting/presetSetting-slice.ts
@@ -1,15 +1,47 @@
-import { createSlice, PayloadAction, Draft } from '@reduxjs/toolkit';
+import {
+  createSlice,
+  PayloadAction,
+  Draft,
+  createAsyncThunk
+} from '@reduxjs/toolkit';
+import { IPresetSetting } from '../../../..//types';
+import {
+  generateMockSetting,
+  presetSettingOptionsMock
+} from '../../../../utils/mock';
 
 export interface PresetSettingInterface {
   activeSetting: number;
   startIndex: number;
   endIndex: number;
+  pending: boolean;
+  error: boolean;
+  settings: IPresetSetting[];
 }
 const initialState: PresetSettingInterface = {
-  activeSetting: 2,
+  activeSetting: 0,
   startIndex: 2,
-  endIndex: 0
+  endIndex: 0,
+  pending: false,
+  error: false,
+  settings: []
 };
+
+export const fetchPresetSetting = createAsyncThunk(
+  'presetSetting/fetchSetting',
+  async (name?: string) => {
+    const response: any = await new Promise((resolve) =>
+      setTimeout(
+        () =>
+          resolve({
+            data: generateMockSetting(name || '')
+          }),
+        300
+      )
+    );
+    return response.data;
+  }
+);
 
 const presetSettingSlice = createSlice({
   name: 'presetSettings',
@@ -50,6 +82,34 @@ const presetSettingSlice = createSlice({
       state.endIndex = action.payload;
       return state;
     }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPresetSetting.pending, (state: PresetSettingInterface) => {
+        state.pending = true;
+        state.error = false;
+        state.settings = [];
+        state.activeSetting = 0;
+      })
+      .addCase(
+        fetchPresetSetting.fulfilled,
+        (
+          state: PresetSettingInterface,
+          action: PayloadAction<IPresetSetting[]>
+        ) => {
+          state.pending = false;
+          state.error = false;
+          state.settings = action.payload;
+          state.endIndex = action.payload.length + 1;
+          state.activeSetting = 2;
+        }
+      )
+      .addCase(fetchPresetSetting.rejected, (state: PresetSettingInterface) => {
+        state.pending = false;
+        state.error = true;
+        state.endIndex = 0;
+        state.activeSetting = 0;
+      });
   }
 });
 
