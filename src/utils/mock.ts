87100c9be33f028ad26b3c1d27f9d3d1fc5a39b1
Phong Vu <caophong.w@gmail.com>
2023-02-16 17:18:01 +0700

    feat(app): mock api call

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -4,6 +4,8 @@ export const MockPresets: IPreset[] = [
   {
     id: 1,
     name: 'Filter 2.1',
+    stage: 'idle',
+    time: '5',
     sensors: {
       t: '53',
       p: '0',
@@ -15,6 +17,8 @@ export const MockPresets: IPreset[] = [
   {
     id: 2,
     name: 'Espresso',
+    stage: 'purge',
+    time: '3',
     sensors: {
       t: '53',
       p: '0',
@@ -26,6 +30,8 @@ export const MockPresets: IPreset[] = [
   {
     id: 3,
     name: 'Mariage Frères',
+    stage: 'home',
+    time: '60',
     sensors: {
       t: '53',
       p: '0',
