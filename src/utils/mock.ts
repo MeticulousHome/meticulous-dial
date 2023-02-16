6e7626f467d0bef7fb531bbc223c3b03852c8818
Phong Vu <caophong.w@gmail.com>
2023-02-16 20:07:17 +0700

    feat(preset setting): update type preset setting

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -4,8 +4,6 @@ export const MockPresets: IPreset[] = [
   {
     id: 1,
     name: 'Filter 2.1',
-    stage: 'idle',
-    time: '5',
     sensors: {
       t: '53',
       p: '0',
@@ -16,8 +14,6 @@ export const MockPresets: IPreset[] = [
   {
     id: 2,
     name: 'Espresso',
-    stage: 'purge',
-    time: '3',
     sensors: {
       t: '53',
       p: '0',
@@ -28,8 +24,6 @@ export const MockPresets: IPreset[] = [
   {
     id: 3,
     name: 'Mariage Frères',
-    stage: 'home',
-    time: '60',
     sensors: {
       t: '53',
       p: '0',
