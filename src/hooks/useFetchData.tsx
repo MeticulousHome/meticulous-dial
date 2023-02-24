adcdd7df1c08485ecdfb11401e1dc04177ff7bba
Phong Vu <caophong.w@gmail.com>
2023-02-25 01:21:16 +0700

    chore(remove comment): remove unused comment

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -2,7 +2,6 @@ import { useContext, useEffect } from 'react';
 import { fetchPreset } from '../components/store/features/preset/preset-slice';
 import { SockerContext } from '../components/store/SockerManager';
 import { fetchPresetSetting } from '../components/store/features/presetSetting/presetSetting-slice';
-// import { setStats } from '../components/store/features/stats/stats-slice';
 
 export function useFetchData() {
   const dispatch = useContext(SockerContext);
@@ -10,36 +9,6 @@ export function useFetchData() {
   useEffect(() => {
     dispatch(fetchPreset());
     dispatch(fetchPresetSetting('Filter 2.1'));
-    // setTimeout(() => {
-    //   dispatch(
-    //     setStats({
-    //       profile: 'Mock Stats',
-    //       name: 'initialize',
-    //       sensors: {
-    //         p: '60',
-    //         t: '20',
-    //         w: '200',
-    //         f: '100'
-    //       },
-    //       time: '200'
-    //     })
-    //   );
-    // }, 3000);
-    // setTimeout(() => {
-    //   dispatch(
-    //     setStats({
-    //       profile: 'Mock Stats',
-    //       name: 'idle',
-    //       sensors: {
-    //         p: '60',
-    //         t: '20',
-    //         w: '200',
-    //         f: '100'
-    //       },
-    //       time: '200'
-    //     })
-    //   );
-    // }, 6000);
   }, []);
 
   // useEffect(() => {
