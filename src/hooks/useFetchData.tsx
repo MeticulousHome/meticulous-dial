9ca01fc7ebe88436c851701096905a01da726c7d
MiguelOsorno <miguel.osornolinares@gmail.com>
2023-02-27 11:51:04 -0600

    feat(src/hooks/use-fetch-data): fetch data when active preset name change

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -2,14 +2,19 @@ import { useContext, useEffect } from 'react';
 import { fetchPreset } from '../components/store/features/preset/preset-slice';
 import { SockerContext } from '../components/store/SockerManager';
 import { fetchPresetSetting } from '../components/store/features/presetSetting/presetSetting-slice';
+import { useAppSelector } from '../components/store/hooks';
 
 export function useFetchData() {
   const dispatch = useContext(SockerContext);
+  const { presets } = useAppSelector((state) => state);
 
   useEffect(() => {
     dispatch(fetchPreset());
-    dispatch(fetchPresetSetting('Filter 2.1'));
   }, []);
+
+  useEffect(() => {
+    dispatch(fetchPresetSetting(presets.activePreset.name));
+  }, [presets.activePreset.name]);
 
   // useEffect(() => {
   //   if (presets.activePreset) {
