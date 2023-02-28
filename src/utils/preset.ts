300d3b75d17c4d2a2a3d69545815b105db182177
Phong Vu <caophong.w@gmail.com>
2023-03-01 01:45:27 +0700

    feat(preset slides): fix animation

---
+++ src/utils/preset.ts
@@ -0,0 +1,34 @@
+import Swiper from 'swiper';
+
+export const handleRemovePresetsAnimation = (swiper: Swiper) => {
+  swiper.slides.forEach((slide) => {
+    slide
+      .querySelector('div')
+      .classList.remove('animation-bounce-left', 'animation-bounce-right');
+  });
+};
+
+export const handleAddPresetAnimation = (swiper: Swiper) => {
+  const { previousIndex, activeIndex, slides } = swiper;
+
+  const animation = activeIndex > previousIndex ? 'left' : 'right';
+
+  swiper.slides[activeIndex]
+    .querySelector('div')
+    .classList.add(`animation-bounce-${animation}`);
+
+  if (slides.length < 2) return;
+  if (activeIndex === 0 || activeIndex === slides.length - 1) {
+    slides[previousIndex]
+      .querySelector('div')
+      .classList.add(`animation-bounce-${animation}`);
+  } else {
+    slides[previousIndex]
+      .querySelector('div')
+      .classList.add(`animation-bounce-${animation}`);
+
+    slides[activeIndex > previousIndex ? activeIndex + 1 : activeIndex - 1]
+      .querySelector('div')
+      .classList.add(`animation-bounce-${animation}`);
+  }
+};
