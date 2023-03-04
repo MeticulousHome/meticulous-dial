27f0d465fe41b53f706de3f32b2e3a0044f313de
Phong Vu <caophong.w@gmail.com>
2023-03-04 11:22:30 +0700

    feat(app): inti assets

--- src/components/store/features/preset/preset-slice.ts
+++ src/components/store/features/preset/preset-slice.ts
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-import { MockPresets } from '../../../../utils/mock';
 
 import { IPreset } from '../../../../types/index';
+import presetsData from '../../../../data/presets.json';
 
 interface GesturesState {
   value: IPreset[];
@@ -28,19 +28,6 @@ const initialState: GesturesState = {
   pending: false,
   error: false
 };
-
-export const fetchPreset = createAsyncThunk('presets/fetchPreset', async () => {
-  const response: any = await new Promise((resolve) =>
-    setTimeout(
-      () =>
-        resolve({
-          data: MockPresets
-        }),
-      300
-    )
-  );
-  return response.data;
-});
 
 const presetSlice = createSlice({
   name: 'presets',
@@ -86,30 +73,6 @@ const presetSlice = createSlice({
       state.activePresetIndex = state.value.length - 1;
       state.activePreset = state.value[state.activePresetIndex];
     }
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchPreset.pending, (state: GesturesState) => {
-        state.pending = true;
-        state.error = false;
-      })
-      .addCase(
-        fetchPreset.fulfilled,
-        (state: GesturesState, action: PayloadAction<IPreset[]>) => {
-          state.pending = false;
-          state.value = action.payload;
-          state.activePresetIndex = 0;
-          if (action.payload[0]) {
-            state.activePreset = action.payload[0];
-          }
-        }
-      )
-      .addCase(fetchPreset.rejected, (state: GesturesState) => {
-        state.value = [];
-        state.activePreset = null;
-        state.pending = false;
-        state.error = true;
-      });
   }
 });
 
--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,24 +1,23 @@
 import { useContext, useEffect } from 'react';
-import { fetchPreset } from '../components/store/features/preset/preset-slice';
 import { SockerContext } from '../components/store/SockerManager';
-import { fetchPresetSetting } from '../components/store/features/presetSetting/presetSetting-slice';
 import { useAppSelector } from '../components/store/hooks';
+import { setActivePreset } from '../components/store/features/preset/preset-slice';
 
 export function useFetchData() {
   const dispatch = useContext(SockerContext);
   const { presets } = useAppSelector((state) => state);
 
   useEffect(() => {
-    dispatch(fetchPreset());
-  }, []);
-
-  useEffect(() => {
-    dispatch(fetchPresetSetting(presets.activePreset.name));
-  }, [presets.activePreset.name]);
-
-  // useEffect(() => {
-  //   if (presets.activePreset) {
-  //     dispatch(fetchPresetSetting(presets.activePreset.name));
-  //   }
-  // }, [presets.activePreset]);
+    if (
+      Array.isArray(presets.value) &&
+      presets.value.length &&
+      presets.activePreset.id === -1
+    ) {
+      let defaultPresetIndex = presets.value.findIndex(
+        (preset) => preset.isDefault === true
+      );
+      defaultPresetIndex = defaultPresetIndex === -1 ? 0 : defaultPresetIndex;
+      setActivePreset(defaultPresetIndex);
+    }
+  }, [presets]);
 }
