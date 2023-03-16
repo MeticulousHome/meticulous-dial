1e70a44978cd8569ff66c2483f3244571e8cd9af
Phong Vu <caophong.w@gmail.com>
2023-03-16 21:45:41 +0700

    feat(src index): change place to save file from src directory to apppath temp

--- src/data/presets.ts
+++ src/data/presets.ts
@@ -5,10 +5,7 @@ export const getPresetsData = async () =>
 
 export const setPresetsData = async (presets: IPreset[]) => {
   const json = JSON.stringify(presets);
-  const response = await window.meticulousAPI.saveFile(
-    './src/data/presets.json',
-    json
-  );
+  const response = await window.meticulousAPI.saveFile('presets.json', json);
 
   if (response) console.log('Oops, there was an error.');
   else console.log('The file has been saved!');
