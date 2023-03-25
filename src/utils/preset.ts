1e8584acc70c73b43b87b1651d11434a37b3924e
Phong Vu <caophong.w@gmail.com>
2023-03-26 03:18:01 +0700

    fix(presets): resolve warnings

--- src/utils/preset.ts
+++ src/utils/preset.ts
@@ -14,22 +14,24 @@ export const handleAddPresetAnimation = (swiper: Swiper) => {
 
   const animation = activeIndex > previousIndex ? 'left' : 'right';
   console.log('active', activeIndex);
-  swiper.slides[activeIndex]
-    .querySelector('div')
-    .classList.add(`animation-bounce-${animation}`);
-
-  if (slides.length < 2) return;
-  if (activeIndex === 0 || activeIndex === slides.length - 1) {
-    slides[previousIndex]
-      .querySelector('div')
-      .classList.add(`animation-bounce-${animation}`);
-  } else {
-    slides[previousIndex]
+  if (swiper.slides[activeIndex]) {
+    swiper.slides[activeIndex]
       .querySelector('div')
       .classList.add(`animation-bounce-${animation}`);
 
-    slides[activeIndex > previousIndex ? activeIndex + 1 : activeIndex - 1]
-      .querySelector('div')
-      .classList.add(`animation-bounce-${animation}`);
+    if (slides.length < 2) return;
+    if (activeIndex === 0 || activeIndex === slides.length - 1) {
+      slides[previousIndex]
+        .querySelector('div')
+        .classList.add(`animation-bounce-${animation}`);
+    } else {
+      slides[previousIndex]
+        .querySelector('div')
+        .classList.add(`animation-bounce-${animation}`);
+
+      slides[activeIndex > previousIndex ? activeIndex + 1 : activeIndex - 1]
+        .querySelector('div')
+        .classList.add(`animation-bounce-${animation}`);
+    }
   }
 };
