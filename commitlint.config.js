f2eb44a7fdf7cc60a627a5669d1d1c9f645a6b9e
Joel Arvidsson <joel@oblador.se>
2023-06-08 17:34:08 +0200

    chore(prettier): format all files with prettier

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,30 +1,9 @@
 module.exports = {
-    "extends": [
-        "@commitlint/config-conventional"
-    ],
-    "rules": {
-        "header-max-length": [
-            2,
-            "always",
-            100
-        ],
-        "scope-case": [
-            2,
-            "always",
-            [
-                "lower-case"
-            ]
-        ],
-        "scope-empty": [
-            2,
-            "never"
-        ],
-        "subject-case": [
-            2,
-            "always",
-            [
-                "lower-case"
-            ]
-        ]
-    }
-}
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', ['lower-case']],
+    'scope-empty': [2, 'never'],
+    'subject-case': [2, 'always', ['lower-case']]
+  }
+};
