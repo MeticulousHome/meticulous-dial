eb61e6d51cd2d37f0156117caace76974ddee568
Juan Antonio Rosales Fuentes <jrosales@Juans-MacBook-Pro.local>
2023-06-13 17:14:39 -0600

    feat(properties): only send lcd properties

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const LCD_EVENTS = {
+  ITALIAN_EVENT: 'italian_1.0',
+  ACTION_EVENT: 'action'
+};
+
+export const LCD_ACTIONS = {
+  START_VALUE: 'start'
+};
--- src/utils/preheat.ts
+++ src/utils/preheat.ts
@@ -1,4 +1,4 @@
-import { IPresetSetting, PressetSettings } from '../types/index';
+import { IItalian, IPresetSetting, PressetSettings } from '../types/index';
 
 interface PayloadProps {
   presset: PressetSettings;
@@ -6,6 +6,26 @@ interface PayloadProps {
 
 const getKeyPresset = (presset: PressetSettings, key: string) => {
   return presset.settings.find((item) => item.key === key);
+};
+
+export const generateSimplePayload = ({ presset }: PayloadProps): IItalian => {
+  const temperature = getKeyPresset(presset, 'temperature');
+  const preinfusion = getKeyPresset(presset, 'pre-infusion');
+  const preheat = getKeyPresset(presset, 'pre-heat');
+  const pressure = getKeyPresset(presset, 'pressure');
+  const purge = getKeyPresset(presset, 'purge');
+  const output = getKeyPresset(presset, 'output');
+
+  return {
+    name: presset.name,
+    automatic_purge: purge?.value === 'automatic',
+    temperature: Number(temperature.value),
+    preinfusion: preinfusion?.value === 'yes',
+    preheat: preheat?.value === 'yes',
+    pressure: Number(pressure.value),
+    out_weight: Number(output.value),
+    source: 'lcd'
+  };
 };
 
 export const generatePayload = ({ presset }: PayloadProps) => {
