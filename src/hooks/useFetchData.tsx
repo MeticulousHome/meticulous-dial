a75997f7659b04bf10ec3a51bfbfd8e375cb8087
Joel Arvidsson <joel@oblador.se>
2023-06-21 20:44:34 +0200

    fix(router): emit actions to socket

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,9 +1,9 @@
-import { useContext, useEffect } from 'react';
-import { SocketContext } from '../components/store/SocketManager';
+import { useEffect } from 'react';
 import { getPresets } from '../components/store/features/preset/preset-slice';
+import { useAppDispatch } from '../components/store/hooks';
 
 export function useFetchData() {
-  const dispatch = useContext(SocketContext);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getPresets());
