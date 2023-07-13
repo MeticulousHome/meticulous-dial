ed9c17d11b7e93c255697be6a354221433062bba
Joel Arvidsson <joel@oblador.se>
2023-07-13 18:34:23 +0200

    feat(barometer): new fonts and dynamic meter

--- src/components/Barometer/Barometer.tsx
+++ src/components/Barometer/Barometer.tsx
@@ -7,19 +7,11 @@ import { useSocket } from '../store/SocketManager';
 import { setScreen } from '../store/features/screens/screens-slice';
 import { LCD_EVENTS } from '../../../src/constants';
 import { resetActiveSetting } from '../store/features/preset/preset-slice';
+import { Meter } from './Meter';
 
 export interface IBarometerProps {
   maxValue?: number;
 }
-
-const getBarNeedlePosition = (pressure: string, maxValue: number) => {
-  const final = (313 * parseFloat(pressure)) / maxValue;
-  const barNeedleRotatePosition = final < 0 ? 0 : final;
-
-  return barNeedleRotatePosition > 313
-    ? 313 + 114
-    : barNeedleRotatePosition + 114; //start position
-};
 
 export function Barometer({ maxValue = 13 }: IBarometerProps): JSX.Element {
   const stats = useAppSelector((state) => state.stats);
@@ -60,20 +52,17 @@ export function Barometer({ maxValue = 13 }: IBarometerProps): JSX.Element {
     }
   });
 
-  const barNeedleRotatePosition = getBarNeedlePosition(
-    stats.sensors.p,
-    maxValue
-  );
-
   return (
     <div className="barometer-container">
-      <div
-        className="bar-needle bar-needle--transition-all"
-        style={{ transform: `rotate(${barNeedleRotatePosition}deg)` }}
-      ></div>
-
+      <Meter
+        min={0}
+        max={maxValue}
+        step={1}
+        value={Number.parseFloat(stats.sensors.p)}
+        className="meter"
+      />
       <div className="bar-needle__content">
-        <div className="pressure">PRESSURE</div>
+        <div className="pressure">Pressure</div>
         <div className="bar-needle__legend">
           <span className="bar-needle__value">
             {formatStatValue(stats.sensors.p, 1)}
@@ -83,40 +72,32 @@ export function Barometer({ maxValue = 13 }: IBarometerProps): JSX.Element {
 
         <div className="columns-grid">
           <div className="column-item">
-            <div>
-              <div className="column-label">TEMP</div>
-              <div className="column-value">
-                {formatStatValue(stats.sensors.t, 1)}
-              </div>
+            <div className="column-label">TEMP</div>
+            <div className="column-value">
+              {formatStatValue(stats.sensors.t, 1)}
+              <div className="column-unit">°C</div>
             </div>
-            <div className="column-data">°C</div>
           </div>
           <div className="column-item">
-            <div>
-              <div className="column-label">WEIGHT</div>
-              <div className="column-value">
-                {formatStatValue(stats.sensors.w, 1)}
-              </div>
+            <div className="column-label">WEIGHT</div>
+            <div className="column-value">
+              {formatStatValue(stats.sensors.w, 1)}
+              <div className="column-unit">g</div>
             </div>
-            <div className="column-data">g</div>
           </div>
           <div className="column-item">
-            <div>
-              <div className="column-label">TIME</div>
-              <div className="column-value">
-                {formatStatValue(stats.time, 1)}
-              </div>
+            <div className="column-label">TIME</div>
+            <div className="column-value">
+              {formatStatValue(stats.time, 1)}
+              <div className="column-unit">sec</div>
             </div>
-            <div className="column-data">sec</div>
           </div>
           <div className="column-item">
-            <div>
-              <div className="column-label">FLOW</div>
-              <div className="column-value">
-                {formatStatValue(stats.sensors.f, 1)}
-              </div>
+            <div className="column-label">FLOW</div>
+            <div className="column-value">
+              {formatStatValue(stats.sensors.f, 1)}
+              <div className="column-unit">ml/s</div>
             </div>
-            <div className="column-data">ml/s</div>
           </div>
         </div>
 
---
+++ src/components/Barometer/Meter.tsx
@@ -0,0 +1,74 @@
+// Basic trigonometry constants
+const CIRCLE = Math.PI * 2;
+const CIRCLE_DEG = 360;
+const CIRCLE_BOTTOM_ANGLE = 0.25 * CIRCLE;
+
+// For the steps/needle we don't want a full circle, but 300 deg
+const ARC_FILL_RATIO = (CIRCLE_DEG - 60) / CIRCLE_DEG;
+const ARC_START_ANGLE =
+  CIRCLE_BOTTOM_ANGLE + ((1 - ARC_FILL_RATIO) * CIRCLE) / 2;
+const ARC_END_ANGLE = ARC_START_ANGLE + ARC_FILL_RATIO * CIRCLE;
+
+// Sizing
+const ARC_SIZE = 480; // Basically window size, but can be anything as long as svg is sized properly
+const ARC_RADIUS = ARC_SIZE / 2;
+const STEP_LENGTH = 13;
+const STEP_EDGE_OFFSET = 19;
+const NEEDLE_LENGTH = ARC_RADIUS - STEP_LENGTH - STEP_EDGE_OFFSET - 10;
+
+interface MeterProps {
+  className?: string;
+  min: number;
+  max: number;
+  value: number;
+  step: number;
+}
+
+export function Meter({ min, max, value, step, className }: MeterProps) {
+  const range = max - min;
+  const steps = range / step + 1;
+  const clampedValue = Math.max(min, Math.min(value, max));
+  const relativeValue = (clampedValue - min) / range;
+  const needleAngle =
+    ARC_START_ANGLE + relativeValue * (ARC_END_ANGLE - ARC_START_ANGLE);
+
+  return (
+    <svg
+      width={ARC_SIZE}
+      height={ARC_SIZE}
+      viewBox={`0 0 ${ARC_SIZE} ${ARC_SIZE}`}
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      {new Array(steps).fill(null).map((_, i) => {
+        const angle =
+          ARC_START_ANGLE +
+          (i / (steps - 1)) * (ARC_END_ANGLE - ARC_START_ANGLE);
+        const x = Math.cos(angle);
+        const y = Math.sin(angle);
+        const start = ARC_RADIUS - STEP_EDGE_OFFSET;
+        const end = start - STEP_LENGTH;
+        return (
+          <path
+            d={`M${ARC_RADIUS + x * start} ${ARC_RADIUS + y * start} L${
+              ARC_RADIUS + x * end
+            } ${ARC_RADIUS + y * end}`}
+            stroke="#4A4A4A"
+            strokeWidth={1}
+          />
+        );
+      })}
+      <path
+        d={`M${ARC_RADIUS} ${ARC_RADIUS} L${
+          ARC_RADIUS + NEEDLE_LENGTH
+        } ${ARC_RADIUS}`}
+        stroke="#F5C444"
+        strokeWidth={4}
+        transform={`rotate(${Math.round(
+          needleAngle * (CIRCLE_DEG / CIRCLE)
+        )}, ${ARC_RADIUS}, ${ARC_RADIUS})`}
+        style={{ transition: 'all 0.3s ease-in' }}
+      />
+    </svg>
+  );
+}
