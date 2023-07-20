f8d1c3cfa6ee9d2bcdeea48c5e6cbea882a4243a
Joel Arvidsson <joel@oblador.se>
2023-07-20 14:29:32 +0200

    fix(presets): transition preset titles horizontally

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -14,6 +14,10 @@ const memoizedRoutes = Object.fromEntries(
   ])
 );
 
+export interface RouteProps {
+  transitioning: boolean;
+}
+
 interface RouterProps {
   currentScreen: ScreenType;
   previousScreen?: ScreenType;
