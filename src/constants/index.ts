c4b7e490d511cd804422bc9e1029f632804b0563
salatielosorno <salatiel.linares@gmail.com>
2023-09-12 17:11:30 -0600

    feat(src/utils/preheat.ts): add kind property to the payload send

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,7 @@
-export const LCD_EVENTS = {
-  ITALIAN_EVENT: 'italian_1.0',
+export type Profile = 'italian_1_0';
+
+export const LCD_EVENTS: { ITALIAN_EVENT: Profile; ACTION_EVENT: 'action' } = {
+  ITALIAN_EVENT: 'italian_1_0',
   ACTION_EVENT: 'action'
 };
 
