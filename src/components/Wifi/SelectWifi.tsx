be9e73ca5e007b71b12fe56220580de70a948695
Phong Vu <caophong.w@gmail.com>
2024-02-13 22:10:08 +0700

    feat(wifi): update select wifi screen swiper

--- src/components/Wifi/SelectWifi.tsx
+++ src/components/Wifi/SelectWifi.tsx
@@ -1,54 +1,28 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
-import { useHandleGestures } from '../../hooks/useHandleGestures';
+import React, { useEffect, useState } from 'react';
+import { SwiperSlide } from 'swiper/react';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { setScreen } from '../store/features/screens/screens-slice';
 import { getWifis, selectWifi } from '../store/features/wifi/wifi-slice';
 import { WifiIcon } from './WifiIcon';
 import { LoadingScreen } from '../LoadingScreen/LoadingScreen';
+import { SwiperWrapper } from '../Swiper/SwiperWrapper';
 
 import './selectWifi.css';
 
 export const SelectWifi = (): JSX.Element => {
-  const [activeIndex, setActiveIndex] = useState(0);
   const [animationStyle, setAnimationStyle] = useState('');
 
-  const swiperRef = useRef<SwiperRef>(null);
-
   const dispatch = useAppDispatch();
-  const { pending, wifiList } = useAppSelector((state) => state.wifi);
+  const { pending, wifiList = [] } = useAppSelector((state) => state.wifi);
 
-  useHandleGestures({
-    left() {
-      setActiveIndex((prev) => Math.max(prev - 1, 0));
-    },
-    right() {
-      setActiveIndex((prev) =>
-        Math.min(
-          prev + 1,
-          (swiperRef?.current?.swiper?.slides || []).length - 1
-        )
-      );
-    },
-    click() {
-      if (activeIndex === wifiList.length) {
-        dispatch(setScreen('connectWifi'));
-      } else {
-        dispatch(selectWifi(wifiList[activeIndex].ssid));
-        dispatch(setScreen('enterWifiPassword'));
-      }
+  const onClick = (_activeId: string, activeIndex: number) => {
+    if (activeIndex === wifiList.length) {
+      dispatch(setScreen('connectWifi'));
+    } else {
+      dispatch(selectWifi(wifiList[activeIndex].ssid));
+      dispatch(setScreen('enterWifiPassword'));
     }
-  });
-
-  useEffect(() => {
-    if (swiperRef.current?.swiper) {
-      try {
-        swiperRef.current.swiper.slideTo(activeIndex);
-      } catch (error) {
-        console.log({ error, location: 'Wifi' });
-      }
-    }
-  }, [activeIndex]);
+  };
 
   useEffect(() => {
     dispatch(getWifis());
@@ -61,26 +35,14 @@ export const SelectWifi = (): JSX.Element => {
   return (
     <div className="main-layout">
       <div className="settings-options">
-        <Swiper
-          ref={swiperRef}
-          slidesPerView={9}
-          allowTouchMove={false}
-          initialSlide={activeIndex}
-          direction="vertical"
-          autoHeight={false}
-          centeredSlides={true}
-          onSlideNextTransitionStart={() => {
-            setAnimationStyle('animation-next');
-          }}
-          onSlidePrevTransitionStart={() => setAnimationStyle('animation-prev')}
-          onSlideChangeTransitionEnd={() => setAnimationStyle('')}
-        >
+        <SwiperWrapper onClick={onClick} setAnimationStyle={setAnimationStyle}>
           {wifiList.length &&
             wifiList.map((network, index) => {
               return (
                 <SwiperSlide
                   className="setting-option-item"
                   key={`option-${index}`}
+                  style={{ height: '60px' }}
                 >
                   <div className={`${animationStyle} network-option`}>
                     <span>{network.ssid}</span>
@@ -89,10 +51,14 @@ export const SelectWifi = (): JSX.Element => {
                 </SwiperSlide>
               );
             })}
-          <SwiperSlide className="setting-option-item" key="cancel">
+          <SwiperSlide
+            className="setting-option-item"
+            key="cancel"
+            style={{ height: '60px' }}
+          >
             <div className={`${animationStyle} network-option`}>cancel</div>
           </SwiperSlide>
-        </Swiper>
+        </SwiperWrapper>
       </div>
       <div className="fade fade-top"></div>
       <div className="fade fade-bottom"></div>
