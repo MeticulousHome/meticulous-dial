db8277b7ed9aa946842b67e596e68519fa55afae
MiguelOsorno <miguel.osornolinares@gmail.com>
2024-03-06 03:25:57 -0600

    feat(pressets): add title

--- src/components/Pressets/Pressets.tsx
+++ src/components/Pressets/Pressets.tsx
@@ -1,11 +1,5 @@
 // Core modules imports are same as usual
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState
-} from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Swiper, SwiperSlide, SwiperRef } from 'swiper/react';
 import SwiperS, { Pagination as PaginationSwiper } from 'swiper';
 
@@ -39,45 +33,64 @@ import { setScreen } from '../store/features/screens/screens-slice';
 import { generateSimplePayload } from '../../utils/preheat';
 import { useSocket } from '../store/SocketManager';
 import { KIND_PROFILE, LCD_EVENT_EMIT } from '../../constants';
-import {
-  circumference,
-  getDashArray,
-  getValue
-} from '../SettingNumerical/Gauge';
-import styled, { RuleSet, css, keyframes } from 'styled-components';
+import { circumference, getDashArray } from '../SettingNumerical/Gauge';
+import styled, { css, keyframes } from 'styled-components';
 
 const radius = 237;
 const transform = `rotate(90, ${radius}, ${radius})`;
 
-const dashValue = getDashArray(0, 100);
-const dashFinalValues = getDashArray(0, 100);
+const TitleCircle = React.memo(
+  ({
+    titleOpacityEnd,
+    titleOpacityInitial,
+    value1,
+    value2
+  }: {
+    value1: number;
+    value2: number;
+    titleOpacityInitial: number;
+    titleOpacityEnd: number;
+  }) => {
+    const fade = keyframes`
+  0%{
+    opacity: ${titleOpacityInitial};
+  }
+  100% {
+    opacity: ${titleOpacityEnd};
+  }
+  `;
 
-// let scroll = keyframes`
-//       0% {
-//         troke-dashoffset: ${dashValue}
-//       }
-//       100% {
-//         troke-dashoffset: ${dashFinalValues}
-//       }`;
+    const fadeAnimation = () => css`
+      ${fade} ${value2 > 0
+        ? (value2 - value1) * 10
+        : value1 * 30}ms linear forwards
+    `;
 
-// const scrollAnimation = () => css`
-//   ${scroll} 2s ease-out forwards
-// `;
+    const Title = styled.h1`
+      animation: ${fadeAnimation};
+    `;
 
-// const SlideTrackContainer = styled.circle``;
+    return (
+      <div className="cicle-container">
+        <Title>Hold to start</Title>
+      </div>
+    );
+  }
+);
 
 const Circle = React.memo(
   ({
     value1,
     value2,
     fillEnd,
-    fillInitial
+    fillInitial,
+    timeFunc
   }: {
     value1: number;
     value2: number;
-    timing: number;
     fillInitial: number;
     fillEnd: number;
+    timeFunc?: 'ease-in' | 'linear';
   }) => {
     const value1Ref = useRef(getDashArray(value1, 100));
     const value2Ref = useRef(getDashArray(value2, 100));
@@ -85,17 +98,17 @@ const Circle = React.memo(
     const scroll = keyframes`
     0% {
       stroke-dasharray: ${value1Ref.current};
-      fill: rgba(0,0,0, ${fillInitial > 0.7 ? 0.7 : fillInitial});
+      fill: rgba(0,0,0, ${Math.min(fillInitial, 0.8)});
     }
     100% {
       stroke-dasharray: ${value2Ref.current};
-      fill: rgba(0,0,0, ${fillEnd > 0.7 ? 0.7 : fillEnd});
+      fill: rgba(0,0,0, ${Math.min(fillEnd, 0.8)});
     }`;
 
     const scrollAnimation = () => css`
       ${scroll} ${value2 > 0
-        ? (value2 - value1) * 20
-        : value1 * 30}ms linear forwards
+        ? (value2 - value1) * 10
+        : value1 * 30}ms ${timeFunc} forwards
     `;
 
     const SlideTrackContainer = styled.circle`
@@ -110,7 +123,6 @@ const Circle = React.memo(
         cy={radius - 3}
         r={radius}
         fill="transparent"
-        // strokeDasharray={getDashArray(100, 100)}
         strokeDashoffset="0"
         strokeLinecap="butt"
         transform={transform}
@@ -123,11 +135,15 @@ export default Circle;
 
 const initialValue = {
   key: 1,
+  key2: 20,
   value1: 0,
   value2: 0,
   timming: 0,
   fillInitial: 0.0,
-  fillEnd: 0.0
+  fillEnd: 0.0,
+  titleOpacityEnd: 0,
+  titleOpacityInitial: 0,
+  timeFunc: 'linear' as 'linear' | 'ease-in'
 };
 
 export function Pressets({ transitioning }: RouteProps): JSX.Element {
@@ -144,11 +160,15 @@ export function Pressets({ transitioning }: RouteProps): JSX.Element {
 
   const [animation, setAnimation] = useState<{
     key: number;
+    key2: number;
     value1: number;
     value2: number;
     timming: number;
     fillInitial: number;
     fillEnd: number;
+    titleOpacityInitial: number;
+    titleOpacityEnd: number;
+    timeFunc: 'linear' | 'ease-in';
   }>(initialValue);
 
   const bubbleDisplay = useAppSelector((state) => state.screen.bubbleDisplay);
@@ -164,11 +184,8 @@ export function Pressets({ transitioning }: RouteProps): JSX.Element {
   const navigationTitleRef = useRef<HTMLDivElement | null>(null);
   const pressetsTitleContentRef = useRef<HTMLDivElement | null>(null);
   const [percentaje, setPercentaje] = useState(0);
-  const percentajeRef = useRef(0);
   const [startCoffe, setStartCoffe] = useState(false);
   const ready = useRef(false);
-
-  // console.log('percentaje', percentaje);
 
   const pressetTitleContenExistValidation = useCallback(() => {
     if (!pressetsTitleContentRef.current) {
@@ -251,9 +268,6 @@ export function Pressets({ transitioning }: RouteProps): JSX.Element {
             // console.log('valueACTUAL', valueACTUAL);
 
             return setPercentaje((prev) => {
-              percentajeRef.current =
-                prev === 0 && valueACTUAL > 0 ? valueACTUAL : prev + 1;
-
               if (!animationInProgress.current) {
                 // console.log('set animation');
 
@@ -262,15 +276,16 @@ export function Pressets({ transitioning }: RouteProps): JSX.Element {
                 }
                 setAnimation((prev2) => ({
                   key: prev2.key + 1 > 10 ? 0 : prev2.key + 1,
+                  key2: prev2.key2 + 1 > 30 ? 20 : prev2.key2 + 1,
                   value1:
                     prev === 0 && valueACTUAL > 0 ? valueACTUAL : prev + 1,
                   value2: 100,
                   timming: 1000,
-                  fillInitial:
-                    valueACTUAL / 100 > 0
-                      ? valueACTUAL / 100 + 0.2
-                      : valueACTUAL,
-                  fillEnd: 0.7
+                  fillInitial: valueACTUAL / 100,
+                  fillEnd: 0.7,
+                  titleOpacityEnd: 0,
+                  titleOpacityInitial: 0,
+                  timeFunc: 'linear'
                 }));
               }
 
@@ -526,29 +541,21 @@ export function Pressets({ transitioning }: RouteProps): JSX.Element {
           setPercentaje(() => {
             setAnimation((prev) => ({
               key: prev.key + 1 > 10 ? 0 : prev.key + 1,
+              key2: prev.key2 + 1 > 30 ? 20 : prev.key2 + 1,
               value1: valueACTUAL,
               value2: 0,
               timming: 0,
-              fillInitial:
-                valueACTUAL / 100 > 0 ? valueACTUAL / 100 + 0.2 : valueACTUAL,
-              fillEnd: 0.0
+              fillInitial: valueACTUAL / 100,
+              fillEnd: 0.0,
+              titleOpacityInitial: Math.min(valueACTUAL / 100, 0.9),
+              titleOpacityEnd: 0,
+              timeFunc: 'ease-in'
             }));
 
             return 0;
           });
         }, 200);
       }
-
-      // const dashValue = getDashArray(percentaje, 100);
-
-      if (percentaje > 0) {
-        percentajeRef.current = percentaje;
-      }
-
-      // for (const [, dato] of circleOne.current.classList.entries()) {
-      //   circleOne.current.classList.remove(dato);
-      // }
-      // circleOne.current.style.strokeDasharray = `${dashValue.toString()}`;
     }
 
     return () => {
@@ -568,28 +575,41 @@ export function Pressets({ transitioning }: RouteProps): JSX.Element {
   return (
     <div className="preset-wrapper">
       <div className="cicle-container">
-        <svg id="svg" width="460" height="460" viewBox="-1 -2 480 480">
-          <circle
-            cx={radius}
-            cy={radius - 3}
-            r={radius}
-            fill="transparent"
-            strokeDasharray={getDashArray(0, 100)}
-            strokeDashoffset="0"
-            transform={transform}
-          ></circle>
+        <div
+          style={{
+            position: 'relative'
+          }}
+        >
+          <TitleCircle
+            key={animation.key2}
+            value1={animation.value1}
+            value2={animation.value2}
+            titleOpacityEnd={animation.titleOpacityEnd}
+            titleOpacityInitial={animation.titleOpacityInitial}
+          />
+          <svg id="svg" width="460" height="460" viewBox="-1 -2 480 480">
+            <circle
+              cx={radius}
+              cy={radius - 3}
+              r={radius}
+              fill="transparent"
+              strokeDasharray={getDashArray(0, 100)}
+              strokeDashoffset="0"
+              transform={transform}
+            ></circle>
 
-          {option.screen === 'HOME' && (
-            <Circle
-              fillEnd={animation.fillEnd}
-              fillInitial={animation.fillInitial}
-              timing={animation.timming}
-              key={animation.key}
-              value1={animation.value1}
-              value2={animation.value2}
-            />
-          )}
-        </svg>
+            {option.screen === 'HOME' && (
+              <Circle
+                timeFunc={animation.timeFunc}
+                fillEnd={animation.fillEnd}
+                fillInitial={animation.fillInitial}
+                key={animation.key}
+                value1={animation.value1}
+                value2={animation.value2}
+              />
+            )}
+          </svg>
+        </div>
       </div>
       {presets.defaultPresetIndex > -1 && (
         <>
