313fddbef81754e8208b1213bf9529bdde966270
Juan Antonio Rosales Fuentes <jrosales@Juans-MacBook-Pro.local>
2024-03-07 14:33:40 -0600

    fix(images): fix static images loaded

--- src/components/Pressets/ProfileImage.tsx
+++ src/components/Pressets/ProfileImage.tsx
@@ -3,15 +3,14 @@ interface ProfileImageProps {
   image?: string;
 }
 
-const exampleImage =
-  'https://s3-alpha-sig.figma.com/img/36d3/5d52/602d3b8648f9810dde10595f3add9e73?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VCd3CoH0u~Keiwux2dMT5Coo7e1th1GEvG0x5SPoQHIpHu4LzkaYt7y8vA3ZU8NmPp7CVeJ0~EQYOoHfSNST-RgHU2KLQ1aqc722fxAgW9YhL2IZpliT4Zm8PDsqSuy6JQcTkgetg3sQpLqyt4tcwMHY67Ltg0r9UxY5azs~FfSnk2pf~Kqcb4fWNK~FV4yUw2muBriz37KBYZKS3wvbcbj4g3aNXp-FNHJsrk4otEjSc4eAh2Z3noe~f9j2G1CWurg90cGOZ-vQtvDz8nY2~qiZpp~Vki0uPC40uVmIOT7imgIyUi~ICaRKg03h6UIvr3jVVgppOxt7abYfLNjc6w__';
+const staticImage = 'assets/images/profile-default.svg';
 
 const borderExample = '#A56751';
 
 export const ProfileImage = ({ image, borderColor }: ProfileImageProps) => {
   return (
     <img
-      src={image || exampleImage}
+      src={image || staticImage}
       alt="image-profile"
       width="164"
       height="164"
