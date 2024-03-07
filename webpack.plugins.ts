9dd0c23430b97164dc6320abcc6da618cc31a8b6
Juan Antonio Rosales Fuentes <jrosales@Juans-MacBook-Pro.local>
2024-03-07 14:33:40 -0600

    fix(images): fix static images loaded

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,22 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { DefinePlugin } from 'webpack';
+import path from 'path';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CopyPlugin = require('copy-webpack-plugin');
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 export const plugins = [
+  new CopyPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, 'src/assets/'),
+        to: path.resolve(__dirname, '.webpack/renderer/assets/')
+      }
+    ]
+  }),
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure'
   }),
