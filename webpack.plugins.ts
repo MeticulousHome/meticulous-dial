6b8fb02c7a3ef2646b4f513b396ab8b7353dc939
angel-zepeda <angel.zepeda0508@gmail.com>
2024-03-07 19:49:38 -0600

    fix(path): fix path order

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,27 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { DefinePlugin } from 'webpack';
+import path from 'path';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CopyPlugin = require('copy-webpack-plugin');
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
+const webpackPathByOs = process.platform === 'linux' ? 'main_window/' : '';
+
 export const plugins = [
+  new CopyPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, 'src/assets/'),
+        to: path.resolve(
+          __dirname,
+          `.webpack/renderer/${webpackPathByOs}assets/`
+        )
+      }
+    ]
+  }),
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure'
   }),
