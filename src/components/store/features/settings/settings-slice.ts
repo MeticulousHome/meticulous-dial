804d841260dbdf0134ee1b763c34aa12d617a4dc
angel-zepeda <angel.zepeda0508@gmail.com>
2024-03-11 08:46:17 -0600

    refactor(settings): use redux thunk

--- src/components/store/features/settings/settings-slice.ts
+++ src/components/store/features/settings/settings-slice.ts
@@ -1,17 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { isAxiosError } from 'axios';
+
+import SettingsApi from '../../../../api/settings';
 
 const initialState = {};
+
+export const fetchSettigns = createAsyncThunk(
+  'settings/fetchSettings',
+  async () => {
+    try {
+      const { data } = await SettingsApi.get();
+      return data;
+    } catch (error) {
+      if (isAxiosError(error)) return error.message;
+      return error;
+    }
+  }
+);
+
+export const updateSettings = createAsyncThunk(
+  'settings/updateSettings',
+  async (body: Record<string, any>) => {
+    try {
+      const { data } = await SettingsApi.update(body);
+      return data;
+    } catch (error) {
+      if (isAxiosError(error)) return error.message;
+      return error;
+    }
+  }
+);
 
 const settingsSlice = createSlice({
   name: 'settings',
   initialState,
-  reducers: {
-    updateSettings: (state, action) => {
-      // This is to store any new setting from backend
-      return action.payload;
-    }
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchSettigns.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(updateSettings.fulfilled, (state, action) => {
+        return action.payload;
+      });
   }
 });
 
-export const { updateSettings } = settingsSlice.actions;
 export default settingsSlice.reducer;
