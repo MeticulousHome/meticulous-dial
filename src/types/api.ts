00d512ad964b3e2d4da0724f5dee616e2eb2c89b
angel-zepeda <angel.zepeda0508@gmail.com>
2024-03-11 08:46:17 -0600

    refactor(settings): use redux thunk

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,15 +1,6 @@
 import { AxiosResponse } from 'axios';
 
-import { GlobalSettings, IPreset } from '.';
-
-export interface ProfileImplementation {
-  get: (id: number | string) => Promise<AxiosResponse>;
-  save: (body: IPreset) => Promise<AxiosResponse>;
-  delete(id: number | string): Promise<AxiosResponse>;
-  loadDataProfile: (data: any) => Promise<AxiosResponse>;
-  loadProfile: (profile_id: number) => Promise<AxiosResponse>;
-  getAll: () => Promise<AxiosResponse>;
-}
+import { GlobalSettings } from '.';
 
 export interface SettingsImplementation {
   get(): Promise<AxiosResponse>;
