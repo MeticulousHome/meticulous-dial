2df37e0c83280289e1b02b69ab7234fb7b01e9ba
Juan Antonio Rosales Fuentes <jrosales@Juans-MacBook-Pro.local>
2024-04-01 14:53:10 -0600

    feat(value): add prehat value

--- src/components/BottomStatus/index.tsx
+++ src/components/BottomStatus/index.tsx
@@ -4,14 +4,22 @@ import './bottom-status.css';
 
 const BottomStatus = ({ hidden }: { hidden: boolean }) => {
   const stats = useAppSelector((state) => state.stats);
+  const { auto_preheat } = useAppSelector((state) => state.settings);
 
   return (
     <div className={`bottom-status bottom__${hidden ? 'fadeOut' : 'fadeIn'}`}>
       <div className="bottom-content">
         <div className="bottom-item">
-          <div className={`status-value ${stats.waterStatus ? 'blue' : 'red'}`}>
+          <div
+            className={`status-value ${
+              stats.waterStatus ? 'blue' : 'red'
+            } relative`}
+          >
             {formatStatValue(stats.sensors.t, 1)}
             <div className="status-unit status-temp-icon">°C</div>
+            <div className="text-small">
+              {auto_preheat > 0 ? `${auto_preheat} °C` : ''}
+            </div>
           </div>
         </div>
         <div className="bottom-item">
