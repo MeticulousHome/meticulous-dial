e2e2bc86c6014ba75a911d20051d254a193f2b59
Juan Antonio Rosales Fuentes <jrosales@Juans-MacBook-Pro.local>
2024-05-15 17:03:26 -0600

    feat(rive): add rive animattions

--- src/components/Barometer/PurgePiston/PurgePiston.tsx
+++ src/components/Barometer/PurgePiston/PurgePiston.tsx
@@ -1,40 +1,211 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
-import lottie, { AnimationItem } from 'lottie-web';
-import './piston.css';
-import piston from './piston.json';
-import blink from './blink.json';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { formatStatValue } from '../../../utils';
-import { setScreen } from '../../../../src/components/store/features/screens/screens-slice';
-import { useSocket } from '../../../../src/components/store/SocketManager';
+// import { useCallback, useEffect, useRef, useState } from 'react';
+// import lottie, { AnimationItem } from 'lottie-web';
+// import './piston.css';
+// import piston from './piston.json';
+// import blink from './blink.json';
+// import { useAppDispatch, useAppSelector } from '../../store/hooks';
+// import { formatStatValue } from '../../../utils';
+// import { setScreen } from '../../../../src/components/store/features/screens/screens-slice';
+// import { useSocket } from '../../../../src/components/store/SocketManager';
 
-const MAX_POSITION = 83;
-const TOTAL_FRAMES = 60.0;
-const NO_FRAMES = 1000;
+// const MAX_POSITION = 83;
+// const TOTAL_FRAMES = 60.0;
+// const NO_FRAMES = 1000;
+
+// export function PurgePiston(): JSX.Element {
+//   const stats = useAppSelector((state) => state.stats);
+//   const screen = useAppSelector((state) => state.screen.value);
+//   const socket = useSocket();
+//   const pistonContainer = useRef<AnimationItem | null>(null);
+//   const pistonAnimator = useRef(null);
+//   const blinkContainer = useRef<AnimationItem | null>(null);
+//   const blinkAnimator = useRef(null);
+
+//   const [initialPosition, setInitialPosition] = useState<number | null>(null);
+//   const [prevPosition, setPrevPosition] = useState<number | null>(null);
+//   const [prevTime, setPrevTime] = useState<number | null>(null);
+//   const firstTime = useRef<boolean>(true);
+//   const statusFirstName = useRef<boolean>(true);
+//   const prevStatus = useRef<string>('');
+//   const [position, setPosition] = useState<number>(-1);
+//   const intervalRef = useRef(null);
+
+//   const dispatch = useAppDispatch();
+
+//   const animateToPosition = useCallback((targetPosition: number) => {
+//     if (pistonContainer.current) {
+//       const startPosition = pistonContainer.current.currentRawFrame;
+//       const startTime = performance.now();
+
+//       const animate = () => {
+//         const elapsed = performance.now() - startTime;
+//         const progress = Math.min(elapsed / 200, 1);
+
+//         const newPosition =
+//           startPosition + (targetPosition - startPosition) * progress;
+
+//         const clampedPosition = Math.max(
+//           0,
+//           Math.min(newPosition, TOTAL_FRAMES)
+//         );
+
+//         if (clampedPosition > TOTAL_FRAMES) {
+//           return;
+//         }
+
+//         pistonContainer.current?.goToAndStop(clampedPosition, true);
+
+//         if (progress < 1) {
+//           requestAnimationFrame(animate);
+//         }
+//       };
+
+//       requestAnimationFrame(animate);
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     blinkContainer.current = lottie.loadAnimation({
+//       container: blinkAnimator.current,
+//       animationData: blink,
+//       renderer: 'svg',
+//       loop: true,
+//       autoplay: true
+//     });
+
+//     if (screen === 'home') {
+//       blinkAnimator.current.style.top = '-206.5px';
+//     }
+
+//     socket.on('actuators', (data: { m_pos: number }) => {
+//       if (data.m_pos < 0) {
+//         return;
+//       }
+//       setPosition(data.m_pos);
+//     });
+//   }, []);
+
+//   const initAnimation = (initial: number) => {
+//     setInitialPosition(initial);
+//     pistonContainer.current = lottie.loadAnimation({
+//       container: pistonAnimator.current,
+//       animationData: piston,
+//       renderer: 'canvas',
+//       loop: false,
+//       autoplay: false
+//     });
+
+//     pistonContainer.current.goToAndStop(
+//       (initial / TOTAL_FRAMES) * NO_FRAMES,
+//       false
+//     );
+//   };
+
+//   useEffect(() => {
+//     if (position === -1) {
+//       return;
+//     }
+
+//     let myPosition = position;
+
+//     if (myPosition > MAX_POSITION) {
+//       myPosition = MAX_POSITION;
+//     }
+
+//     const currentPosition = (myPosition / MAX_POSITION) * TOTAL_FRAMES;
+
+//     if (firstTime.current) {
+//       firstTime.current = false;
+//       statusFirstName.current = false;
+
+//       if (!pistonContainer.current) {
+//         initAnimation(currentPosition);
+//       }
+
+//       return;
+//     }
+
+//     if (
+//       initialPosition !== null &&
+//       prevPosition !== null &&
+//       prevTime !== null
+//     ) {
+//       animateToPosition(currentPosition);
+//     }
+
+//     setPrevPosition(currentPosition);
+//     setPrevTime(Date.now());
+//   }, [position, animateToPosition, initialPosition]);
+
+//   useEffect(() => {
+//     if (!statusFirstName.current) {
+//       if (prevStatus.current === 'home' || prevStatus.current === 'purge') {
+//         if (stats.name === 'idle') {
+//           dispatch(setScreen('pressets'));
+//         }
+//       }
+
+//       prevStatus.current = stats.name;
+//     }
+//   }, [stats.name]);
+
+//   useEffect(() => {
+//     intervalRef.current = setInterval(() => {
+//       if (position === -1) {
+//         dispatch(setScreen('pressets'));
+//       }
+//     }, 2000);
+
+//     return () => {
+//       clearInterval(intervalRef.current);
+//     };
+//   }, [position]);
+
+//   return (
+//     <div className="piston-container">
+//       <div className="piston-purge-container center">
+//         <div className="values">
+//           <div className="value">
+//             {formatStatValue(stats.sensors.p, 1)}
+//             <span>bar</span>
+//           </div>
+//           <div id="blink" ref={blinkAnimator} className="lottie" />
+//           <div id="piston" ref={pistonAnimator} className="lottie" />
+//           <div className="value">
+//             {formatStatValue(stats.sensors.f, 1)}
+//             <span>ml/s</span>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+import { useRive } from '@rive-app/react-canvas';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useSocket } from '../../../../src/components/store/SocketManager';
+import { useAppDispatch } from '../../../../src/components/store/hooks';
+import { setScreen } from '../../../../src/components/store/features/screens/screens-slice';
+const TOTAL_LEVEL = 85.0;
 
 export function PurgePiston(): JSX.Element {
-  const stats = useAppSelector((state) => state.stats);
-  const screen = useAppSelector((state) => state.screen.value);
-  const socket = useSocket();
-  const pistonContainer = useRef<AnimationItem | null>(null);
-  const pistonAnimator = useRef(null);
-  const blinkContainer = useRef<AnimationItem | null>(null);
-  const blinkAnimator = useRef(null);
-
   const [initialPosition, setInitialPosition] = useState<number | null>(null);
   const [prevPosition, setPrevPosition] = useState<number | null>(null);
   const [prevTime, setPrevTime] = useState<number | null>(null);
-  const firstTime = useRef<boolean>(true);
-  const statusFirstName = useRef<boolean>(true);
-  const prevStatus = useRef<string>('');
   const [position, setPosition] = useState<number>(-1);
   const intervalRef = useRef(null);
-
+  const socket = useSocket();
   const dispatch = useAppDispatch();
 
-  const animateToPosition = useCallback((targetPosition: number) => {
-    if (pistonContainer.current) {
-      const startPosition = pistonContainer.current.currentRawFrame;
+  const { rive, RiveComponent } = useRive({
+    src: 'assets/water.riv',
+    stateMachines: 'State Machine',
+    autoplay: true
+  });
+
+  const animateToPosition = useCallback(
+    (targetPosition: number, cp: number) => {
+      const startPosition = cp;
       const startTime = performance.now();
 
       const animate = () => {
@@ -44,16 +215,18 @@ export function PurgePiston(): JSX.Element {
         const newPosition =
           startPosition + (targetPosition - startPosition) * progress;
 
-        const clampedPosition = Math.max(
-          0,
-          Math.min(newPosition, TOTAL_FRAMES)
-        );
+        const clampedPosition = Math.max(0, Math.min(newPosition, TOTAL_LEVEL));
 
-        if (clampedPosition > TOTAL_FRAMES) {
+        if (clampedPosition > TOTAL_LEVEL) {
           return;
         }
 
-        pistonContainer.current?.goToAndStop(clampedPosition, true);
+        if (rive) {
+          console.log(clampedPosition);
+          rive
+            .stateMachineInputs('State Machine')
+            .find((e) => e.name === 'Level').value = clampedPosition;
+        }
 
         if (progress < 1) {
           requestAnimationFrame(animate);
@@ -61,22 +234,11 @@ export function PurgePiston(): JSX.Element {
       };
 
       requestAnimationFrame(animate);
-    }
-  }, []);
+    },
+    [rive]
+  );
 
   useEffect(() => {
-    blinkContainer.current = lottie.loadAnimation({
-      container: blinkAnimator.current,
-      animationData: blink,
-      renderer: 'svg',
-      loop: true,
-      autoplay: true
-    });
-
-    if (screen === 'home') {
-      blinkAnimator.current.style.top = '-206.5px';
-    }
-
     socket.on('actuators', (data: { m_pos: number }) => {
       if (data.m_pos < 0) {
         return;
@@ -84,70 +246,6 @@ export function PurgePiston(): JSX.Element {
       setPosition(data.m_pos);
     });
   }, []);
-
-  const initAnimation = (initial: number) => {
-    setInitialPosition(initial);
-    pistonContainer.current = lottie.loadAnimation({
-      container: pistonAnimator.current,
-      animationData: piston,
-      renderer: 'canvas',
-      loop: false,
-      autoplay: false
-    });
-
-    pistonContainer.current.goToAndStop(
-      (initial / TOTAL_FRAMES) * NO_FRAMES,
-      false
-    );
-  };
-
-  useEffect(() => {
-    if (position === -1) {
-      return;
-    }
-
-    let myPosition = position;
-
-    if (myPosition > MAX_POSITION) {
-      myPosition = MAX_POSITION;
-    }
-
-    const currentPosition = (myPosition / MAX_POSITION) * TOTAL_FRAMES;
-
-    if (firstTime.current) {
-      firstTime.current = false;
-      statusFirstName.current = false;
-
-      if (!pistonContainer.current) {
-        initAnimation(currentPosition);
-      }
-
-      return;
-    }
-
-    if (
-      initialPosition !== null &&
-      prevPosition !== null &&
-      prevTime !== null
-    ) {
-      animateToPosition(currentPosition);
-    }
-
-    setPrevPosition(currentPosition);
-    setPrevTime(Date.now());
-  }, [position, animateToPosition, initialPosition]);
-
-  useEffect(() => {
-    if (!statusFirstName.current) {
-      if (prevStatus.current === 'home' || prevStatus.current === 'purge') {
-        if (stats.name === 'idle') {
-          dispatch(setScreen('pressets'));
-        }
-      }
-
-      prevStatus.current = stats.name;
-    }
-  }, [stats.name]);
 
   useEffect(() => {
     intervalRef.current = setInterval(() => {
@@ -161,22 +259,25 @@ export function PurgePiston(): JSX.Element {
     };
   }, [position]);
 
-  return (
-    <div className="piston-container">
-      <div className="piston-purge-container center">
-        <div className="values">
-          <div className="value">
-            {formatStatValue(stats.sensors.p, 1)}
-            <span>bar</span>
-          </div>
-          <div id="blink" ref={blinkAnimator} className="lottie" />
-          <div id="piston" ref={pistonAnimator} className="lottie" />
-          <div className="value">
-            {formatStatValue(stats.sensors.f, 1)}
-            <span>ml/s</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  useEffect(() => {
+    if (rive !== null) {
+      const cp = rive
+        .stateMachineInputs('State Machine')
+        .find((e) => e.name === 'Level').value as number;
+
+      if (
+        initialPosition !== null &&
+        prevPosition !== null &&
+        prevTime !== null
+      ) {
+        animateToPosition(position, cp);
+      }
+
+      setPrevPosition(position);
+      setPrevTime(Date.now());
+      setInitialPosition(position);
+    }
+  }, [position, animateToPosition, initialPosition, rive]);
+
+  return <RiveComponent />;
 }
