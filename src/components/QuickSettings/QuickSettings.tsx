ba58e64c7e6794b7f55f0cdbfcd76c4044737c53
Mimoja <mimoja@mimoja.de>
2024-07-23 03:37:00 +0200

    fix(lint): fix nits
    Signed-off-by: Mimoja <mimoja@mimoja.de>

--- src/components/QuickSettings/QuickSettings.tsx
+++ src/components/QuickSettings/QuickSettings.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import './quick-settings.css';
 import { useHandleGestures } from '../../hooks/useHandleGestures';
