833c9bef58c7b9b323a0ffd9ffdabaa51589b803
Mimoja <mimoja@mimoja.de>
2024-07-23 21:12:05 +0200

    feat(profiles): retry profile fetching on failure
    Signed-off-by: Mimoja <mimoja@mimoja.de>

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,13 +1,22 @@
 import { useEffect } from 'react';
 import { getPresets } from '../components/store/features/preset/preset-slice';
 import { getConfig as getWifiConfig } from '../components/store/features/wifi/wifi-slice';
-import { useAppDispatch } from '../components/store/hooks';
+import { useAppDispatch, useAppSelector } from '../components/store/hooks';
 
 export function useFetchData() {
   const dispatch = useAppDispatch();
+  const presetsState = useAppSelector((state) => state.presets.status);
 
   useEffect(() => {
     dispatch(getPresets({}));
     dispatch(getWifiConfig());
   }, []);
+
+  useEffect(() => {
+    if (presetsState === 'failed') {
+      setTimeout(() => {
+        dispatch(getPresets({}));
+      }, 1000);
+    }
+  }, [presetsState]);
 }
