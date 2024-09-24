b308786844bd81f2700edf2d87e6319a5604afd9
Angel-Padilla <angel.p.lopez@outlook.com>
2024-09-24 17:20:43 -0600

    feat(config): add master acaia calibration

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,17 @@
 import Api from '@meticulous-home/espresso-api';
+// import '@meticulous-home/espresso-api'
+import { APIError } from '@meticulous-home/espresso-api';
 
 export const api = new Api(
   undefined,
   process.env.SERVER_URL || 'http://localhost:8080/'
 );
+
+export const startMasterCalibration = async () => {
+  try {
+    const { data } = await api.executeAction('scale_master_calibration');
+    return data;
+  } catch (error) {
+    console.error('Start profile error: ', error.message);
+  }
+};
