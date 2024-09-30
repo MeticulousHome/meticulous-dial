34867dc74e3c41f9d90f738addd8d797f9121187
Arturo Escobar <s.arturoec@gmail.com>
2024-09-30 15:05:50 -0600

    chore(pressets): improve custom hook readability and add documentation annotations

--- src/hooks/usePressets.tsx
+++ src/hooks/usePressets.tsx
@@ -13,11 +13,61 @@ export const PROFILES_QUERY_KEY = 'profiles';
 export const LAST_PROFILE_QUERY_KEY = 'lastProfile';
 
 /**
+ * Receives a `profile` and returns it with the `settings[]` property added. The content of this property is adjusted according to the variables that the profile contains.
+ * @param {Profile} profile - Profile to which you will add the settings.
+ * @returns {ProfileValue} profile modified with the `settings[]` property added.
+ */
+const addSettingsToProfile = (profile: Profile) => ({
+  ...profile,
+  settings: [
+    {
+      id: 1,
+      type: 'text',
+      key: 'name',
+      label: 'name',
+      value: profile.name,
+      isInternal: true
+    },
+    {
+      id: 2,
+      type: 'numerical',
+      key: 'temperature',
+      label: 'temperature',
+      value: profile.temperature || 85,
+      unit: '°c',
+      isInternal: true
+    },
+    {
+      id: 3,
+      type: 'numerical',
+      key: 'output',
+      label: 'output',
+      value: profile.final_weight || 36,
+      unit: 'g',
+      isInternal: true
+    },
+    {
+      id: 4,
+      type: 'image',
+      key: 'image',
+      label: 'Select image',
+      value: profile.display.image.replace('/api/v1/profile/image/', ''),
+      isInternal: true
+    },
+    ...addVariablesToSettings({
+      variables: profile.variables,
+      nextId: 5
+    })
+  ]
+});
+
+/**
  * Return an object with the properties `isLoading`, `isError`, `isFetching`, `isPending`, and `profiles`.
  * The array `profiles` contains the profiles and the last brewed profile combined into a single array.
  * If the last brewed profile hasn’t been deleted, it is marked in the profiles list. If it was deleted,
  * then it is marked and added to the end of the resulting list.
- * @params none
+ * @param none
+ * @returns {Object} an object that contains information about the state of the queries, the unified profiles list, the index of the last profile, and a flag indicating whether it is known or not.
  */
 export function usePressets(): {
   isLoading: boolean;
@@ -25,6 +75,8 @@ export function usePressets(): {
   isFetching: boolean;
   isPending: boolean;
   profiles: Profile[];
+  lastProfileIndex: number;
+  isLastProfileKnown: boolean;
 } {
   return useQueries({
     queries: [
@@ -32,53 +84,7 @@ export function usePressets(): {
         queryKey: [PROFILES_QUERY_KEY],
         queryFn: getProfiles,
         placeholderData: keepPreviousData,
-        select: (data: Profile[]) =>
-          data.map((profile) => ({
-            ...profile,
-            settings: [
-              {
-                id: 1,
-                type: 'text',
-                key: 'name',
-                label: 'name',
-                value: profile.name,
-                isInternal: true
-              },
-              {
-                id: 2,
-                type: 'numerical',
-                key: 'temperature',
-                label: 'temperature',
-                value: profile.temperature || 85,
-                unit: '°c',
-                isInternal: true
-              },
-              {
-                id: 3,
-                type: 'numerical',
-                key: 'output',
-                label: 'output',
-                value: profile.final_weight || 36,
-                unit: 'g',
-                isInternal: true
-              },
-              {
-                id: 4,
-                type: 'image',
-                key: 'image',
-                label: 'Select image',
-                value: profile.display.image.replace(
-                  '/api/v1/profile/image/',
-                  ''
-                ),
-                isInternal: true
-              },
-              ...addVariablesToSettings({
-                variables: profile.variables,
-                nextId: 5
-              })
-            ]
-          })),
+        select: (data: Profile[]) => data.map(addSettingsToProfile),
         refetchOnReconnect: 'always',
         refetchOnWindowFocus: false
       },
@@ -88,52 +94,7 @@ export function usePressets(): {
         placeholderData: keepPreviousData,
         select: (data: LastProfileIdent) => ({
           ...data,
-          profile: {
-            ...data.profile,
-            settings: [
-              {
-                id: 1,
-                type: 'text',
-                key: 'name',
-                label: 'name',
-                value: data.profile.name,
-                isInternal: true
-              },
-              {
-                id: 2,
-                type: 'numerical',
-                key: 'temperature',
-                label: 'temperature',
-                value: data.profile.temperature || 85,
-                unit: '°c',
-                isInternal: true
-              },
-              {
-                id: 3,
-                type: 'numerical',
-                key: 'output',
-                label: 'output',
-                value: data.profile.final_weight || 36,
-                unit: 'g',
-                isInternal: true
-              },
-              {
-                id: 4,
-                type: 'image',
-                key: 'image',
-                label: 'Select image',
-                value: data.profile.display.image.replace(
-                  '/api/v1/profile/image/',
-                  ''
-                ),
-                isInternal: true
-              },
-              ...addVariablesToSettings({
-                variables: data.profile.variables,
-                nextId: 5
-              })
-            ]
-          }
+          profile: addSettingsToProfile(data.profile)
         }),
         refetchOnReconnect: 'always',
         refetchOnWindowFocus: false
@@ -144,37 +105,42 @@ export function usePressets(): {
 
       const profiles = profilesResult?.data || [];
       const lastProfileBrewed = lastProfileResult?.data?.profile;
+      const {
+        combinedProfiles,
+        lastProfilePotentialIndex,
+        isLastProfileKnown
+      } = useMemo(() => {
+        const lastProfilePotentialIndex = profiles.findIndex(
+          (profile) => profile.id === lastProfileBrewed.id
+        );
 
-      const lastProfilePotentialIndex = profiles.findIndex(
-        (profile) => profile.id === lastProfileBrewed.id
-      );
+        const isLastProfileKnown =
+          lastProfilePotentialIndex >= 0 &&
+          equal(profiles[lastProfilePotentialIndex], lastProfileBrewed);
 
-      const isLastProfileKnown =
-        lastProfilePotentialIndex >= 0 &&
-        equal(profiles[lastProfilePotentialIndex], lastProfileBrewed);
-
-      // Aquí usas useMemo para calcular combinedProfiles solo cuando las dependencias cambian
-      const combinedProfiles = useMemo(() => {
-        return isLastProfileKnown
+        const combinedProfiles = isLastProfileKnown
           ? profiles.map((profile, index) =>
               index === lastProfilePotentialIndex
                 ? { ...profile, isLast: true }
                 : profile
             )
           : [...profiles, { ...lastProfileBrewed, isLast: true }];
-      }, [
-        profiles,
-        lastProfileBrewed,
-        lastProfilePotentialIndex,
-        isLastProfileKnown
-      ]);
+
+        return {
+          combinedProfiles,
+          lastProfilePotentialIndex,
+          isLastProfileKnown
+        };
+      }, [profiles, lastProfileBrewed]);
 
       return {
         isLoading: profilesResult.isLoading || lastProfileResult.isLoading,
         isError: profilesResult.isError || lastProfileResult.isError,
         isFetching: profilesResult.isFetching || lastProfileResult.isFetching,
         isPending: profilesResult.isPending || lastProfileResult.isPending,
-        profiles: combinedProfiles
+        profiles: combinedProfiles,
+        lastProfileIndex: lastProfilePotentialIndex,
+        isLastProfileKnown
       };
     }
   });
