1131f4e1ed430c879cb17a005cdff8f60a334672
Angel-Padilla <angel.p.lopez@outlook.com>
2024-10-11 18:46:42 -0600

    refactor(advanced.tsx): remove os_status settings item

--- src/components/Settings/Advanced/Advanced.tsx
+++ src/components/Settings/Advanced/Advanced.tsx
@@ -50,12 +50,6 @@ export const AdvancedSettings = () => {
       visible: true
     },
     {
-      key: 'os_update',
-      label: 'OS Status',
-      value: globalSettings.update_channel,
-      visible: true
-    },
-    {
       key: 'save',
       label: 'Save',
       visible: true
@@ -115,11 +109,6 @@ export const AdvancedSettings = () => {
           case 'set_update_channel':
             dispatch(
               setBubbleDisplay({ visible: true, component: 'updateChannel' })
-            );
-            break;
-          case 'os_update':
-            dispatch(
-              setBubbleDisplay({ visible: true, component: 'OSStatus' })
             );
             break;
           case 'device_info':
