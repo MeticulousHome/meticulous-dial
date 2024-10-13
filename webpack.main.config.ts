dfa2cb81c371a95705512b4baf55e4b81162c32f
Mimoja <mimoja@meticuloushome.com>
2024-10-13 07:33:06 -0600

    Revert "fix(webpack): compile preload script"
    This reverts commit 85e81aca54ee8ce10e01c32ccafc14c0f70ff20d.

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -1,25 +1,18 @@
 import type { Configuration } from 'webpack';
 
 import { rules } from './webpack.rules';
-import * as path from 'path';
+
 export const mainConfig: Configuration = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: {
-    index: './src/index.ts',
-    preload: './src/preload.ts'
-  },
+  entry: './src/index.ts',
   // Put your normal webpack config below here
   module: {
     rules
   },
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json']
-  },
-  output: {
-    path: path.resolve(__dirname, '.webpack/main'), // Output both main and preload to this directory
-    filename: '[name].js' // Will output main.js and preload.js
   }
 };
