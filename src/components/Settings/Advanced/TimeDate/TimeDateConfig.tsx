207d73b09a03c02c49ddf76fabd3fa3440b85dd6
Angel-Padilla <angel.p.lopez@outlook.com>
2024-11-26 19:51:18 -0600

    feat(datetime): add config submenu
    chore(zoneselector): delete unused file
    
    chore(datetime): add space-between to move icon to the right
    
    feat(datetime): add config submenu
    
    chore(datetime): add space-between to move icon to the right

---
+++ src/components/Settings/Advanced/TimeDate/TimeDateConfig.tsx
@@ -0,0 +1,145 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import '../../../QuickSettings/quick-settings.css';
+import { useHandleGestures } from '../../../../hooks/useHandleGestures';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
+import {
+  setBubbleDisplay,
+  setScreen
+} from '../../../store/features/screens/screens-slice';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import '../../../OSStatus/OSStatus.css';
+
+import { SettingsItem } from '../../../../types';
+
+const defaultSettings: SettingsItem[] = [
+  {
+    key: 'set_timezone',
+    label: 'Set time zone',
+    visible: true,
+    value: false
+  },
+  {
+    key: 'set_date_time',
+    label: 'Set date & time',
+    visible: true,
+    value: false
+  },
+  {
+    key: 'back',
+    label: 'Back',
+    visible: true
+  }
+];
+
+export function TimeDate(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const bubbleDisplay = useAppSelector((state) => state.screen.bubbleDisplay);
+  const [swiper, setSwiper] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [settings, setSettings] = useState(defaultSettings);
+
+  const [counterESGG, setCounterESGG] = useState(0);
+
+  useHandleGestures(
+    {
+      context() {
+        setActiveIndex(1);
+        dispatch(
+          setBubbleDisplay({
+            visible: !bubbleDisplay.visible,
+            component: !bubbleDisplay.visible ? 'quick-settings' : null
+          })
+        );
+      },
+      left() {
+        setActiveIndex((prev) => Math.max(prev - 1, 0));
+        setCounterESGG(0);
+      },
+      right() {
+        setActiveIndex((prev) => Math.min(prev + 1, settings.length - 1));
+        if (settings[activeIndex].key === 'exit') {
+          setCounterESGG(counterESGG + 1);
+        }
+      },
+      pressDown() {
+        switch (settings[activeIndex].key) {
+          case 'set_timezone': {
+            dispatch(
+              setBubbleDisplay({ visible: true, component: 'timeZoneConfig' })
+            );
+            break;
+          }
+          case 'conset_date_time': {
+            dispatch(
+              setBubbleDisplay({ visible: true, component: 'timeDate' }) //Does nothing for now
+            );
+            break;
+          }
+          case 'back': {
+            dispatch(
+              setBubbleDisplay({ visible: true, component: 'advancedSettings' })
+            );
+            break;
+          }
+        }
+      }
+    },
+    !bubbleDisplay.visible
+  );
+
+  useEffect(() => {
+    setSettings(defaultSettings);
+  }, []);
+
+  useEffect(() => {
+    if (swiper) {
+      swiper.slideTo(activeIndex, 0, false);
+    }
+  }, [activeIndex, swiper]);
+
+  useEffect(() => {
+    if (counterESGG >= 20) {
+      console.log('Easter Egg on');
+      dispatch(setBubbleDisplay({ visible: false, component: null }));
+      dispatch(setScreen('snake'));
+    }
+  }, [counterESGG]);
+
+  const getSettingClasses = useCallback((isActive: boolean) => {
+    return `
+      settings-item ${isActive ? 'active-setting' : ''}
+      `;
+  }, []);
+
+  return (
+    <div className="main-quick-settings">
+      <Swiper
+        onSwiper={setSwiper}
+        slidesPerView={8}
+        allowTouchMove={false}
+        direction="vertical"
+        spaceBetween={25}
+        autoHeight={false}
+        centeredSlides={true}
+        initialSlide={activeIndex}
+        style={{ paddingLeft: '29px', top: '-4px' }}
+      >
+        {settings.map((setting, index: number) => {
+          const isActive = index === activeIndex;
+          return (
+            <div key={`option-${index}-${setting.key}`}>
+              <SwiperSlide
+                className={getSettingClasses(isActive, setting.key)}
+                key={`option-${index}-${setting.key}`}
+              >
+                <div className="text-container">{setting.label}</div>
+              </SwiperSlide>
+            </div>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+}
