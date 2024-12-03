5d82fc6a54278b08ce23b2c47e597183bf0bfe81
Angel-Padilla <angel.p.lopez@outlook.com>
2024-12-02 18:30:00 -0600

    feat(countrysettings): filtered country list request and display
    A filtered list of countries that start with the letter selected on the
    `selectLetterCountry` display is requested to the backend and displayed
    for the user to be selected. This selected country is to be used to filter
    specific timezones

--- src/components/Settings/Advanced/TimeDate/TimeZone/CountrySettings.tsx
+++ src/components/Settings/Advanced/TimeDate/TimeZone/CountrySettings.tsx
@@ -1,66 +1,78 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useAppDispatch, useAppSelector } from '../../../../store/hooks';
-import { MOCK_TZ_JSON, TimezoneData } from './mockTzJson';
 import { useEffect, useMemo, useState } from 'react';
 import { useHandleGestures } from '../../../../../hooks/useHandleGestures';
 import { Option, TextContainer, Title } from './Timezone.styled';
 import { setScreen } from '../../../../store/features/screens/screens-slice';
 import { setCountry } from '../../../../store/features/settings/settings-slice';
-
-export const filterTimezonesByLetter = (data: TimezoneData, letter: string) => {
-  return Object.entries(data)
-    .filter(([country]) => country !== 'ETC' && country !== 'LINKS')
-    .filter(
-      ([country]) => country.charAt(0).toUpperCase() === letter.toUpperCase()
-    )
-    .map(([country, timezones]) => ({ [country]: timezones }));
-};
+import { getTimezoneRegion, isAPIError } from '../../../../../api/api';
+import { Regions } from '@meticulous-home/espresso-api';
 
 export default function CountrySettings() {
   const [swiper, setSwiper] = useState(null);
   const [animationStyle, setAnimationStyle] = useState('');
   const [activeIndex, setActiveIndex] = useState(0);
 
+  const [availableCountries, setAvailableCountries] = useState<Regions>({
+    countries: []
+  });
+
   const countryLetter = useAppSelector((state) => state.settings.countryLetter);
   const bubbleDisplay = useAppSelector((state) => state.screen.bubbleDisplay);
 
   const dispatch = useAppDispatch();
 
-  const availableCountries = useMemo(
-    () => filterTimezonesByLetter(MOCK_TZ_JSON, countryLetter),
-    [countryLetter]
-  );
-
   useEffect(() => {
     if (swiper) swiper.slideTo(activeIndex);
   }, [activeIndex, swiper]);
 
-  const slides = useMemo(
-    () => [
-      <SwiperSlide className="presset-option-item" key={`option-back`}>
-        <div className={`${animationStyle}`}>
-          <Option isactive={activeIndex === 0}>Back</Option>
-        </div>
-      </SwiperSlide>,
-      ...availableCountries.map((country, index) => {
-        const [name] = Object.keys(country);
-        const isactive = index + 1 === activeIndex;
-        return (
-          <SwiperSlide
-            className="presset-option-item"
-            key={`option-${index + 1}`}
-          >
-            <div className={`${animationStyle}`}>
-              <TextContainer neednoWrap={name.length > 11} isactive={isactive}>
-                <Option isactive={isactive}>{name}</Option>
-              </TextContainer>
-            </div>
-          </SwiperSlide>
-        );
-      })
-    ],
-    [activeIndex, animationStyle]
-  );
+  useEffect(() => {
+    console.log('requesting countries');
+    getTimezoneRegion('countries', countryLetter.toLowerCase()).then(
+      (result) => {
+        setAvailableCountries(isAPIError(result) ? { countries: [] } : result);
+      }
+    );
+  }, []);
+
+  const slides = useMemo(() => {
+    console.log(availableCountries);
+    if (availableCountries['countries'].length == 0) {
+      return [
+        <SwiperSlide className="presset-option-item" key={`option-back`}>
+          <div className={`${animationStyle}`}>
+            <Option isactive={activeIndex === 0}>Back</Option>
+          </div>
+        </SwiperSlide>
+      ];
+    } else {
+      return [
+        <SwiperSlide className="presset-option-item" key={`option-back`}>
+          <div className={`${animationStyle}`}>
+            <Option isactive={activeIndex === 0}>Back</Option>
+          </div>
+        </SwiperSlide>,
+        ...availableCountries['countries'].map((country, index) => {
+          const isactive = index + 1 === activeIndex;
+          return (
+            <SwiperSlide
+              className="presset-option-item"
+              key={`option-${index + 1}`}
+            >
+              <div className={`${animationStyle}`}>
+                <TextContainer
+                  neednoWrap={country.length > 11}
+                  isactive={isactive}
+                >
+                  <Option isactive={isactive}>{country}</Option>
+                </TextContainer>
+              </div>
+            </SwiperSlide>
+          );
+        })
+      ];
+    }
+  }, [activeIndex, animationStyle, availableCountries]);
 
   useHandleGestures(
     {
@@ -80,7 +92,7 @@ export default function CountrySettings() {
         if (activeIndex === 0) {
           dispatch(setScreen('selectLetterCountry'));
         } else {
-          dispatch(setCountry(availableCountries[activeIndex - 1]));
+          dispatch(setCountry(availableCountries.countries[activeIndex - 1]));
           dispatch(setScreen('timeZoneSettings'));
         }
       }
