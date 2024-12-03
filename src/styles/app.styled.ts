9d3da386ccf742b472804e2bdc8655d464e7248d
Arturo Escobar <s.arturoec@gmail.com>
2024-12-03 13:50:28 -0600

    refactor(styles): add config to apply common and global styles

---
+++ src/styles/app.styled.ts
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+export const MainCanvas = styled.div`
+  position: relative;
+  width: 480px;
+  height: 480px;
+  max-width: 480px;
+  max-height: 480px;
+  overflow: hidden;
+`;
