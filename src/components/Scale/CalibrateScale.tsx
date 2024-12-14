7e4cf42dcc99009b0dd89623fce5943aa3e507e7
Arturo Escobar <s.arturoec@gmail.com>
2024-12-13 19:40:42 -0600

    feat(calibrationscreen): add new screen for scale calibration

---
+++ src/components/Scale/CalibrateScale.tsx
@@ -0,0 +1,229 @@
+import { styled, css } from 'styled-components';
+import DripTray from './DripTray';
+import Weight from './Weight';
+import { useAppSelector, useAppDispatch } from '../store/hooks';
+import { useHandleGestures } from '../../hooks/useHandleGestures';
+
+import { useEffect, useState } from 'react';
+import {
+  setBubbleDisplay,
+  setScreen
+} from '../store/features/screens/screens-slice';
+import { useSocket } from '../store/SocketManager';
+
+type TransitionProps = {
+  isTaring: boolean;
+};
+
+type TitleProps = {
+  color?: string;
+  fontSize?: string;
+  fontWeight?: string;
+};
+
+//470g - 530g is the range allowed for calibration in acaia scales
+//This logic is temporary since the method of calibration will change. For example, it will be done with 100g instead of 500g.
+const LOWER_RANGE_ALLOWED = 470;
+const UPPER_RANGE_ALLOWED = 530;
+
+const HorizontalWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+`;
+
+const VerticalWrapper = styled.div`
+  height: 100%;
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+`;
+
+const Overlay = styled.div<TransitionProps>`
+  width: 277px;
+  height: 50px;
+  position: absolute;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 1),
+    rgba(0, 0, 0, 0.9)
+  );
+  overflow: hidden;
+  filter: blur(10px);
+  top: ${(props) => (props.isTaring ? '165px' : '140px')};
+  transition:
+    top 0.5s ease,
+    opacity 0.3s ease;
+  transition-delay: 0.8s;
+`;
+
+const Title = styled.div<TitleProps>`
+  border: 1px solid #5d5d5d;
+  border-radius: 30px;
+  line-height: 1.4;
+  position: absolute;
+  letter-spacing: 3.2px;
+  text-align: center;
+  text-transform: uppercase;
+  top: 60px;
+  left: 50%;
+  transform: translateX(-50%);
+  padding-inline: 1rem;
+  & > * {
+    color: ${(props) => props.color || 'inherit'};
+    font-size: ${(props) => props.fontSize || 'inherit'};
+    font-weight: ${(props) => props.fontWeight || 'inherit'};
+  }
+`;
+
+const BottomStatus = styled.div`
+  position: absolute;
+  bottom: 70px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+`;
+
+const Label = styled.div`
+  letter-spacing: 5px;
+  text-transform: uppercase;
+  font-size: 25px;
+  position: relative;
+  height: 25px;
+  width: 100%;
+`;
+
+const Text = styled.span<{ color?: string; show: boolean }>`
+  opacity: ${(props) => (props.show ? '1' : '0')};
+  color: ${(props) => props.color || 'inherit'};
+  transition: opacity 0.5s ease;
+  transition-delay: 0.8s;
+  position: absolute;
+  left: 0;
+  right: 0;
+  text-align: center;
+`;
+
+const SVGContainer = css`
+  width: 100%;
+  position: absolute;
+  transition:
+    top 0.5s ease,
+    opacity 0.5s ease;
+  transition-delay: 0.8s;
+`;
+
+const DripTrayContainer = styled.div<TransitionProps>`
+  ${SVGContainer}
+  max-width: 277px;
+  top: ${(props) => (props.isTaring ? '190px' : '170px')};
+  z-index: -10;
+`;
+
+const WeightContainer = styled.div<TransitionProps>`
+  ${SVGContainer}
+  max-width: 160px;
+  top: ${(props) => (props.isTaring ? '108px' : '60px')};
+  opacity: ${(props) => (props.isTaring ? 1 : 0)};
+`;
+
+const Grams = styled.span`
+  color: #0866ff;
+  font-size: 38px;
+  font-weight: 200;
+  letter-spacing: 7.6px;
+`;
+
+const formatWeight = (weight: string) => {
+  const numericWeight = parseInt(weight, 10);
+  if (numericWeight < 0) {
+    const absoluteWeight = Math.abs(numericWeight).toString().padStart(3, '0');
+    return `-${absoluteWeight}`;
+  }
+  return numericWeight.toString().padStart(3, '0');
+};
+
+export default function CalibrateScale() {
+  const socket = useSocket();
+  const dispatch = useAppDispatch();
+
+  const { w: weight } = useAppSelector((state) => state.stats.sensors);
+  const formattedWeight = formatWeight(weight);
+  const [isTaring, setIsTaring] = useState(false);
+  const [isCalibrated, setIsCalibrated] = useState(false);
+  const parsedWeight = parseInt(weight);
+
+  //This logic is temporary since the method of calibration will change. For example, it will be done with 100g instead of 500g.
+  const notAllowCalibrate =
+    parsedWeight <= 0 ||
+    parsedWeight < LOWER_RANGE_ALLOWED ||
+    parsedWeight > UPPER_RANGE_ALLOWED;
+
+  useEffect(() => {
+    if (!isCalibrated) return;
+
+    const timeoutId = setTimeout(() => {
+      dispatch(setBubbleDisplay({ visible: true, component: null }));
+      dispatch(setScreen('pressets'));
+    }, 5000);
+
+    return () => clearTimeout(timeoutId);
+  }, [isCalibrated]);
+
+  useHandleGestures({
+    singleTare() {
+      setIsTaring(true);
+    },
+    doubleClick() {
+      dispatch(setBubbleDisplay({ visible: false, component: null }));
+      dispatch(setScreen('pressets'));
+    },
+    pressDown() {
+      if (notAllowCalibrate) return;
+      socket.emit('calibrate', '');
+      setIsCalibrated(true);
+    }
+  });
+
+  return (
+    <>
+      <Title color="rgba(255, 255, 255, 0.8)">
+        <span>calibrate scale</span>
+      </Title>
+      <HorizontalWrapper>
+        <VerticalWrapper>
+          <Overlay isTaring={isTaring} />
+          <DripTrayContainer isTaring={isTaring}>
+            <DripTray />
+          </DripTrayContainer>
+          <WeightContainer isTaring={isTaring}>
+            <Weight />
+          </WeightContainer>
+          <BottomStatus>
+            <Grams>{formattedWeight}g</Grams>
+            <Label>
+              <Text show={!isTaring && !isCalibrated}>set zero</Text>
+              <Text
+                show={isTaring && !isCalibrated}
+                color={
+                  notAllowCalibrate
+                    ? 'rgba(255, 255, 255, 0.3)'
+                    : 'rgba(255, 255, 255, 1)'
+                }
+              >
+                calibrate
+              </Text>
+              <Text show={isCalibrated}>done</Text>
+            </Label>
+          </BottomStatus>
+        </VerticalWrapper>
+      </HorizontalWrapper>
+    </>
+  );
+}
---
+++ src/components/Scale/DripTray.tsx
@@ -0,0 +1,61 @@
+export default function DripTray() {
+  return (
+    <svg viewBox="0 0 281 120" fill="none">
+      <path
+        d="M45.4744 1.72656H235.53L279.165 87.6453H1.83984L45.4744 1.72656Z"
+        stroke="#0866FF"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M6.88562 79.1941L274.964 79.1941"
+        stroke="#0866FF"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M11.4302 70.4619L270.267 70.4619"
+        stroke="#0866FF"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M15.0574 61.7298L265.579 61.7298"
+        stroke="#0866FF"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M19.3726 52.9976L261.541 52.9976"
+        stroke="#0866FF"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M24.163 44.2654L257.013 44.2655"
+        stroke="#0866FF"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M28.4814 35.5333L253.058 35.5333"
+        stroke="#0866FF"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M32.7246 26.8011L247.967 26.8012"
+        stroke="#0866FF"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M37.1053 18.069L243.684 18.069"
+        stroke="#0866FF"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M41.8301 9.33679L239.321 9.33681"
+        stroke="#0866FF"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M1.36328 88.4502V118.689H279.646V88.4502"
+        stroke="#0866FF"
+        strokeWidth="1.5"
+      />
+    </svg>
+  );
+}
