1e453ee4fd43d2b68d8a9222869005f99208fb48
Mimoja <mimoja@mimoja.de>
2025-01-15 17:26:55 +0100

    feat(splash): rework splash screen into ready_animation with new animation files
    Signed-off-by: Mimoja <mimoja@mimoja.de>

--- src/components/ReadyAnimation/ReadyAnimation.tsx
+++ src/components/ReadyAnimation/ReadyAnimation.tsx
@@ -3,38 +3,85 @@ import { useEffect, useRef } from 'react';
 import { setScreen } from '../store/features/screens/screens-slice';
 import { useAppDispatch } from '../store/hooks';
 import './ReadyAnimation.css';
-import ReadyAnimationData from './ReadyAnimation.json';
+
+import LoadingAnimation from './LoadingAnimation.json';
+import ReadyAnimation1Data from './ReadyAnimation1.json';
+import ReadyAnimation2Data from './ReadyAnimation2.json';
+import { useFetchData } from '../../hooks/useFetchData';
 
 export function ReadyAnimation(): JSX.Element {
   const dispatch = useAppDispatch();
 
-  const readyAnimation = useRef<AnimationItem | null>(null);
-  const readyDiv = useRef<HTMLDivElement | null>(null);
+  const animation = useRef<AnimationItem | null>(null);
+  const animationDiv = useRef<HTMLDivElement | null>(null);
 
-  function handleAnimationCompleted(): void {
+  function handleReadyAnimation2Completed(): void {
+    animation.current?.destroy();
     dispatch(setScreen('pressets'));
   }
 
+  function handleReadyAnimation1Completed(): void {
+    animation.current?.destroy();
+    animation.current = Lottie.loadAnimation({
+      container: animationDiv.current,
+      animationData: ReadyAnimation2Data,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true
+    });
+    animation.current.setSubframe(true);
+    animation.current.addEventListener(
+      'complete',
+      handleReadyAnimation2Completed
+    );
+  }
+
+  function finishLoadingAnimation(): void {
+    animation.current?.destroy();
+    animation.current = Lottie.loadAnimation({
+      container: animationDiv.current,
+      animationData: ReadyAnimation1Data,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true
+    });
+    animation.current.setSubframe(true);
+    animation.current.addEventListener(
+      'complete',
+      handleReadyAnimation1Completed
+    );
+  }
+
+  useFetchData(() => {
+    finishLoadingAnimation();
+  });
+
   useEffect(() => {
-    if (!readyAnimation.current) {
-      readyAnimation.current = Lottie.loadAnimation({
-        container: readyDiv.current,
-        animationData: ReadyAnimationData,
-        renderer: 'svg',
-        loop: false,
-        autoplay: true
-      });
-      readyAnimation.current.setSubframe(true);
-      readyAnimation.current.addEventListener(
-        'complete',
-        handleAnimationCompleted
-      );
+    if (!animationDiv.current) {
+      return;
     }
-  }, [readyDiv]);
+
+    if (!animation.current) {
+      animation.current = Lottie.loadAnimation({
+        container: animationDiv.current,
+        animationData: LoadingAnimation,
+        renderer: 'svg',
+        loop: true,
+        autoplay: false
+      });
+      animation.current.firstFrame = 380;
+      animation.current.totalFrames = 150;
+
+      animation.current.goToAndPlay(3, true);
+    }
+    return () => {
+      animation.current?.destroy();
+    };
+  }, [animationDiv]);
 
   return (
     <div className="ready-container center">
-      <div id="animation" ref={readyDiv} className="animation" />
+      <div id="animation" ref={animationDiv} className="animation" />
     </div>
   );
 }
