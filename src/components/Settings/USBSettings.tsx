1c32966921225a9f7ee0e25200531939f4916f5c
Mimoja <git@mimoja.de>
2025-01-25 17:53:24 +0100

    feat(usb): add explanation text to the usb settings
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/components/Settings/USBSettings.tsx
+++ src/components/Settings/USBSettings.tsx
@@ -1,6 +1,4 @@
 import { useCallback, useEffect, useState } from 'react';
-import 'swiper/css';
-import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { SettingsKey, USB_MODE } from '@meticulous-home/espresso-api';
 import { useHandleGestures } from '../../hooks/useHandleGestures';
@@ -9,52 +7,73 @@ import { SettingsItem } from '../../types';
 import { marqueeIfNeeded } from '../shared/MarqueeValue';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { setBubbleDisplay } from '../store/features/screens/screens-slice';
+import { MenuAnnotation } from './MenuAnnotation';
 
 export const USBSettings = () => {
   const dispatch = useAppDispatch();
   const { data: globalSettings } = useSettings();
   const updateSettings = useUpdateSettings();
-  const [swiper, setSwiper] = useState(null);
   const [activeIndex, setActiveIndex] = useState(0);
   const bubbleDisplay = useAppSelector((state) => state.screen.bubbleDisplay);
 
-  const settings: SettingsItem[] = [
+  type StaticSettingsItem = SettingsItem & {
+    useableWidthPercentage?: number;
+  };
+
+  const settings: StaticSettingsItem[] = [
     {
       key: 'client',
       label: 'Client / USB-C Network',
-      visible: true
+      useableWidthPercentage: 87
     },
     {
       key: 'host',
       label: 'Host (Charging)',
-      visible: true
+      useableWidthPercentage: 81
     },
     {
       key: 'dual_role',
-      label: 'Dual Role / Undescided',
-      visible: true
+      label: 'DRP (advanced only)',
+      useableWidthPercentage: 77
+    },
+    {
+      key: 'back',
+      label: 'Back'
     }
   ];
 
+  useEffect(() => {
+    if (globalSettings) {
+      const index = settings.findIndex(
+        (setting) => setting.key === globalSettings.usb_mode
+      );
+      setActiveIndex(index);
+    }
+  }, [globalSettings.usb_mode]);
+
   const showValue = useCallback(
-    (isActive: boolean, item: SettingsItem) => {
+    (isActive: boolean, item: StaticSettingsItem) => {
       if (!item) return <></>;
       let val = item.label.toUpperCase();
       if (globalSettings) {
-        switch (item.key) {
-          default:
-            if (typeof globalSettings[item.key as SettingsKey] === 'boolean') {
-              val = globalSettings[item.key as SettingsKey]
-                ? val + ': ENABLED'
-                : val + ': DISABLED';
-            }
+        if (typeof globalSettings[item.key as SettingsKey] === 'boolean') {
+          val = globalSettings[item.key as SettingsKey]
+            ? val + ': ENABLED'
+            : val + ': DISABLED';
         }
 
-        return marqueeIfNeeded({ enabled: isActive, val });
+        return marqueeIfNeeded({
+          enabled: isActive,
+          val,
+          len: 18,
+          forceWidth: item.useableWidthPercentage + '%'
+        });
       }
     },
+
     [globalSettings]
   );
+
   useHandleGestures(
     {
       left() {
@@ -64,56 +83,78 @@ export const USBSettings = () => {
         setActiveIndex((prev) => Math.min(prev + 1, settings.length - 1));
       },
       pressDown() {
-        const activeUsbMode = settings[activeIndex].key as USB_MODE;
-        updateSettings.mutate({
-          usb_mode: activeUsbMode
-        });
-        dispatch(setBubbleDisplay({ visible: true, component: 'settings' }));
+        const activeItem = settings[activeIndex].key;
+        switch (activeItem) {
+          case 'back':
+            dispatch(
+              setBubbleDisplay({ visible: true, component: 'settings' })
+            );
+            break;
+          default: {
+            updateSettings.mutate({
+              usb_mode: activeItem as USB_MODE
+            });
+            dispatch(
+              setBubbleDisplay({ visible: true, component: 'settings' })
+            );
+            break;
+          }
+        }
       }
     },
     !bubbleDisplay.visible
   );
 
-  useEffect(() => {
-    if (swiper) {
-      swiper.slideTo(activeIndex, 0, false);
-    }
-  }, [activeIndex, swiper]);
-
   return (
-    <div className="main-quick-settings">
-      <Swiper
-        onSwiper={setSwiper}
-        slidesPerView={8}
-        allowTouchMove={false}
-        direction="vertical"
-        spaceBetween={25}
-        autoHeight={false}
-        centeredSlides={true}
-        initialSlide={activeIndex}
-        style={{ paddingLeft: '29px', top: '-4px' }}
+    <div className="main-quick-settings settings-explanation-container">
+      <div className="settings-explanation">
+        <div className="settings-explanation-shaper-left" />
+        <div className="settings-explanation-shaper-right" />
+        <span>
+          Use <strong>Client</strong> Mode to connect to the machine directly
+          via USB-C.
+          <br />A USB-C dongle is required to charge a connected Device when in{' '}
+          <strong>Client</strong> mode.
+        </span>
+        <div style={{ marginTop: '10px' }}>
+          <span>
+            Devices which only support <b>client</b> mode (such as USB flash
+            drives) will only work when the machine is set to{' '}
+            <strong>Host</strong>.
+          </span>
+        </div>
+      </div>
+      <div
+        className="settings-fixed-item-container"
+        style={{ marginBottom: '50px' }}
       >
         {settings.map((item, index: number) => {
           const isActive = index === activeIndex;
+          const isSelectedMode = globalSettings?.usb_mode === item.key;
+          console.log(globalSettings?.usb_mode, item.key);
+          const width = item.useableWidthPercentage || 90;
           return (
-            <SwiperSlide
+            <div
               key={index}
-              className={`settings-item ${isActive ? 'active-setting' : ''}`}
+              className={`settings-fixed-item  settings-item ${isActive ? 'active-setting' : ''}`}
+              style={{
+                marginBottom: '5px',
+                width: `${width}%`,
+                paddingRight: `${90 - width}%`
+              }}
             >
-              <div style={{ height: '30px' }}>
-                <div className="settings-entry text-container">
-                  <span
-                    className="settings-text"
-                    style={{ wordBreak: 'break-word' }}
-                  >
-                    {showValue(isActive, item)}
-                  </span>
-                </div>
-              </div>
-            </SwiperSlide>
+              <span className="settings-fixed-item-text ">
+                {showValue(isActive, item)}
+              </span>
+              {isSelectedMode && (
+                <MenuAnnotation style={{ marginLeft: 10 }}>
+                  current
+                </MenuAnnotation>
+              )}
+            </div>
           );
         })}
-      </Swiper>
+      </div>
     </div>
   );
 };
