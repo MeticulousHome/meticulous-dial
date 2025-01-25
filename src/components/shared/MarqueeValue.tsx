740a5abfbea9dca4d505795d4c583132b47663c5
Mimoja <git@mimoja.de>
2025-01-25 17:44:53 +0100

    feat(marquee): convert marquee helper to a styled component
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/components/shared/MarqueeValue.tsx
+++ src/components/shared/MarqueeValue.tsx
@@ -1,13 +1,40 @@
+import { styled, keyframes, css } from 'styled-components';
+
 interface Props {
   enabled: boolean;
   val: string;
   len?: number;
+  forceWidth?: number | string;
 }
 
-export const marqueeIfNeeded = ({ enabled, val, len = 24 }: Props) => {
-  if (enabled && val.length > len) {
-    return <span className="animate-marquee">{val}</span>;
+const createAnimation = (width: string) => keyframes`
+  0% {
+    transform: translateX(0%);
   }
+  100% {
+    transform: translateX(calc(${width} - 100%));
+  }
+`;
 
-  return val;
+const AnimateMarquee = styled.span<{ width: string }>`
+  position: relative;
+  display: inline-block;
+  ${({ width }) => css`
+    animation: ${createAnimation(width)} 6s infinite alternate ease-in-out;
+  `}
+`;
+
+export const marqueeIfNeeded = ({
+  enabled,
+  val,
+  len = 24,
+  forceWidth = 320 // Provide a default if not passed in
+}: Props) => {
+  if (enabled && val.length > len) {
+    if (typeof forceWidth === 'number') {
+      forceWidth = `${forceWidth}px`;
+    }
+    return <AnimateMarquee width={forceWidth}>{val}</AnimateMarquee>;
+  }
+  return <>{val}</>;
 };
