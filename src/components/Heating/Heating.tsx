fcde28ec13f2c0d4af6704926ed7aa2ef9b494e3
Mimoja <git@mimoja.de>
2025-02-07 19:06:28 +0100

    feat(heating): cache the target temperature and estiamte the remaining heating time
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/components/Heating/Heating.tsx
+++ src/components/Heating/Heating.tsx
@@ -1,4 +1,11 @@
-import { CSSProperties, useEffect } from 'react';
+import {
+  CSSProperties,
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState
+} from 'react';
 import { styled } from 'styled-components';
 import { SwitchTransition, CSSTransition } from 'react-transition-group';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
@@ -10,15 +17,22 @@ import {
   ModularFooter,
   ModularLeft,
   ModularRight,
-  ModularRightOptions,
   ModularScreen
 } from '../ModularScreen/ModularScreen';
 import { formatTime } from '../../utils';
-import { useSettings, useUpdateSettings } from '../../hooks/useSettings';
 
-const TimeLeft = styled.div`
+const TimeLeftSeconds = styled.div`
   font-family: 'ABC Diatype Mono';
   font-size: 40px;
+  font-weight: 300;
+  line-height: 1;
+  letter-spacing: -0.02em;
+  text-align: center;
+`;
+
+const TimeLeftText = styled.div`
+  font-family: 'ABC Diatype Mono';
+  font-size: 20px;
   font-weight: 300;
   line-height: 1;
   letter-spacing: -0.02em;
@@ -48,41 +62,77 @@ const StatusLabel = styled.span`
 
 const transitionDuration = 600;
 
-const OPTIONS = [
-  {
-    id: 'auto',
-    label: 'Auto brew'
-  },
-  {
-    id: 'manual',
-    label: 'Push to brew'
-  }
-] as const;
+// const OPTIONS = [
+//   {
+//     id: 'auto',
+//     label: 'Auto brew'
+//   },
+//   {
+//     id: 'manual',
+//     label: 'Push to brew'
+//   }
+// ] as const;
 
 export const Heating = () => {
   const dispatch = useAppDispatch();
   const waterStatus = useAppSelector((state) => state.stats.waterStatus);
-  const preheatTimeLeft = useAppSelector(
-    (state) => state.stats.preheatTimeLeft
-  );
   const temperature = useAppSelector((state) =>
     Math.round(state.stats.sensors.t)
   );
 
-  const temperatureTarget = useAppSelector(
+  // The stages dont necessarily correctly report the temperature target
+  // so we need to cache it with the state below
+  const temperatureTargetStatus = useAppSelector(
     (state) => state.stats.setpoints.temperature
   );
+
+  const [temperatureTarget, setTemperatureTarget] = useState(
+    temperatureTargetStatus
+  );
+  const preheatTimeLeft = useCallback(() => {
+    if (!temperatureTargetStatus) {
+      return <TimeLeftText>stabelizing...</TimeLeftText>;
+    }
+    const diff = temperatureTarget - Math.max(20, temperature);
+
+    if (diff <= 2) {
+      return <TimeLeftText>a few seconds</TimeLeftText>;
+    }
+    if (diff <= 5) {
+      return <TimeLeftText>less than 30 seconds</TimeLeftText>;
+    }
+
+    const stabilizingTime = 20;
+    const heatingTime = 200; // in seconds
+    const maxDiff = 80; // 100°C - 20°C
+    const timePerDegree = heatingTime / maxDiff;
+    const timeIncrements = 15; // in seconds
+    const timeLeft =
+      Math.ceil((diff * timePerDegree + stabilizingTime) / timeIncrements) *
+      timeIncrements;
+    return <TimeLeftSeconds>{formatTime(timeLeft)}</TimeLeftSeconds>;
+  }, [temperatureTarget, temperature, temperatureTargetStatus]);
+
   const statsName = useAppSelector((state) => state.stats.name);
   const heatingFinished = statsName === 'click to start';
 
-  const { data: globalSettings } = useSettings();
-  const updateSettings = useUpdateSettings();
+  // const { data: globalSettings } = useSettings();
+  // const updateSettings = useUpdateSettings();
 
   useEffect(() => {
     if (statsName === 'idle') {
       dispatch(setScreen('pressets'));
     }
   }, [statsName]);
+
+  useEffect(() => {
+    if (
+      !!temperatureTargetStatus &&
+      temperatureTargetStatus !== temperatureTarget
+    ) {
+      setTemperatureTarget(temperatureTargetStatus);
+    }
+  }, [temperatureTargetStatus]);
 
   const transitionStyle: CSSProperties = {
     transform: `translateX(${heatingFinished ? 10 + BUBBLES_CONTAINER_WIDTH / 2 : 0}px)`,
@@ -95,7 +145,7 @@ export const Heating = () => {
         <BubbleAnimation temperature={temperature} waterStatus={waterStatus} />
       </ModularLeft>
       <ModularRight style={transitionStyle}>
-        <CSSTransition
+        {/* <CSSTransition
           in={!heatingFinished}
           unmountOnExit
           timeout={transitionDuration}
@@ -108,7 +158,7 @@ export const Heating = () => {
               updateSettings.mutate({ auto_start_shot: value === 'auto' });
             }}
           />
-        </CSSTransition>
+        </CSSTransition> */}
         <div
           style={{
             display: 'flex',
@@ -166,7 +216,7 @@ export const Heating = () => {
               waterStatus && (
                 <>
                   <Label>Estimated time</Label>
-                  <TimeLeft>{formatTime(preheatTimeLeft)}</TimeLeft>
+                  {preheatTimeLeft()}
                 </>
               )
             )}
