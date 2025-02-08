47e622552cf63279b8276a779450a430a18a5215
Arturo Escobar <s.arturoec@gmail.com>
2025-02-08 10:25:07 -0600

    refactor(device-info): apply new context menu design with styled-components

--- src/components/Settings/Advanced/DeviceInfoScreen.tsx
+++ src/components/Settings/Advanced/DeviceInfoScreen.tsx
@@ -1,22 +1,68 @@
-import { useEffect, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-
+import { useMemo, useState } from 'react';
 import { useHandleGestures } from '../../../hooks/useHandleGestures';
 import { useAppSelector, useAppDispatch } from '../../store/hooks';
 import { setBubbleDisplay } from '../../store/features/screens/screens-slice';
-import { marqueeIfNeeded } from '../../shared/MarqueeValue';
 import { useDeviceInfo } from '../../../hooks/useDeviceOSStatus';
 import { DeviceInfo } from '@meticulous-home/espresso-api';
 import { LoadingScreen } from '../../LoadingScreen/LoadingScreen';
 
+import Styled, {
+  VIEWPORT_HEIGHT,
+  MARQUEE_MIN_TEXT_LENGTH
+} from '../../../styles/utils/mixins';
+import { calculateOptionPosition } from '../../../styles/utils/calculateOptionPosition';
+
+interface RepositoryEntry {
+  branch: string;
+  commit: string;
+}
+
+interface RepositoryInfo {
+  [key: string]: RepositoryEntry;
+}
+
+interface ExtendedDeviceInfo extends DeviceInfo {
+  mainVoltage: number;
+  batch_number: string;
+  build_date: string;
+  image_build_channel: string;
+  repository_info: RepositoryInfo;
+}
+
 export const DeviceInfoScreen = () => {
   const dispatch = useAppDispatch();
-  const { data: deviceInfo, isLoading } = useDeviceInfo();
-  const [swiper, setSwiper] = useState(null);
   const [activeIndex, setActiveIndex] = useState(0);
   const bubbleDisplay = useAppSelector((state) => state.screen.bubbleDisplay);
-  const settingsKeys = Object.keys(deviceInfo || []).concat('back');
+
+  const { data: deviceInfo, isLoading } = useDeviceInfo();
+
+  const updatedDeviceInfo = useMemo(() => {
+    if (!deviceInfo || isLoading) return [];
+
+    const { repository_info, ...basicData } = deviceInfo as ExtendedDeviceInfo;
+
+    const basicInfo = Object.entries(basicData).map(([key, value]) => ({
+      key,
+      label: `${key}: ${value || 'UNSET'}`
+    }));
+
+    const keysToFilter = ['backend', 'dial', 'firmware'];
+
+    const repositoryInfo = repository_info
+      ? Object.entries(repository_info)
+          .filter(([key]) => keysToFilter.includes(key))
+          .map(([key, { branch, commit }]) => ({
+            key,
+            label: `${key}: ${branch} (${commit})`
+          }))
+      : [];
+
+    return [
+      ...basicInfo,
+      ...repositoryInfo,
+      ...[{ key: 'back', label: 'Back' }]
+    ];
+  }, [deviceInfo, isLoading]);
 
   useHandleGestures(
     {
@@ -24,10 +70,12 @@ export const DeviceInfoScreen = () => {
         setActiveIndex((prev) => Math.max(prev - 1, 0));
       },
       right() {
-        setActiveIndex((prev) => Math.min(prev + 1, settingsKeys.length - 1));
+        setActiveIndex((prev) =>
+          Math.min(prev + 1, updatedDeviceInfo.length - 1)
+        );
       },
       pressDown() {
-        const activeItem = settingsKeys[activeIndex];
+        const activeItem = updatedDeviceInfo[activeIndex].key;
         switch (activeItem) {
           case 'back':
             dispatch(
@@ -42,58 +90,58 @@ export const DeviceInfoScreen = () => {
     },
     !bubbleDisplay.visible
   );
+  const optionPositionOutter = useMemo(
+    () =>
+      calculateOptionPosition({
+        activeOptionIdx: activeIndex,
+        settings: updatedDeviceInfo
+      }),
+    [activeIndex, updatedDeviceInfo]
+  );
 
-  useEffect(() => {
-    if (swiper) {
-      swiper.slideTo(activeIndex, 0, false);
-    }
-  }, [activeIndex, swiper]);
+  const optionPositionInner = useMemo(
+    () =>
+      calculateOptionPosition({
+        activeOptionIdx: activeIndex,
+        adjustmentFn: (position) => position - VIEWPORT_HEIGHT / 2,
+        settings: updatedDeviceInfo
+      }),
+    [activeIndex, updatedDeviceInfo]
+  );
 
   if (isLoading) {
     return <LoadingScreen />;
   }
 
   return (
-    <div className="main-quick-settings">
-      <Swiper
-        onSwiper={setSwiper}
-        slidesPerView={8}
-        allowTouchMove={false}
-        direction="vertical"
-        spaceBetween={16}
-        autoHeight={false}
-        centeredSlides={true}
-        initialSlide={activeIndex}
-        style={{ paddingLeft: '29px', top: '-4px' }}
-      >
-        {Object.entries(settingsKeys).map((item, index: number) => {
-          const isActive = index === activeIndex;
-          const name = item[1] as keyof DeviceInfo;
-          let entry = deviceInfo[name];
-          const displayName = name.replace(/_/g, ' ');
-          if (typeof entry === 'object') {
-            entry = 'available via the API';
-          }
-          return (
-            <SwiperSlide
-              key={index}
-              className={`settings-item ${isActive ? 'active-setting' : ''}`}
-            >
-              <div className="settings-entry text-container">
-                <span
-                  className="settings-text"
-                  style={{ wordBreak: 'break-word' }}
-                >
-                  {marqueeIfNeeded({
-                    val: `${displayName}: ${entry !== undefined ? `${entry || 'UNSET'}` : ''}`,
-                    enabled: isActive
-                  })}
-                </span>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </div>
+    <Styled.SettingsContainer>
+      <Styled.Viewport>
+        <Styled.OptionsContainer $translateY={optionPositionOutter}>
+          {updatedDeviceInfo.map((option) => (
+            <Styled.Option key={option.key}>
+              <span>{option.label}</span>
+            </Styled.Option>
+          ))}
+        </Styled.OptionsContainer>
+        <Styled.ActiveIndicator>
+          <Styled.OptionsContainer
+            $translateY={optionPositionInner}
+            $isInner={true}
+          >
+            {updatedDeviceInfo.map((option, index) => (
+              <Styled.Option
+                key={option.key}
+                $isMarquee={
+                  activeIndex === index &&
+                  option.label.length > MARQUEE_MIN_TEXT_LENGTH
+                }
+              >
+                <span>{option.label}</span>
+              </Styled.Option>
+            ))}
+          </Styled.OptionsContainer>
+        </Styled.ActiveIndicator>
+      </Styled.Viewport>
+    </Styled.SettingsContainer>
   );
 };
