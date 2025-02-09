81409f92d3baba20887a9e49c9c1db778d53d022
Arturo Escobar <s.arturoec@gmail.com>
2025-02-09 14:43:13 -0600

    refactor(advanced-settings): apply new context menu design with styled-components

--- src/components/Settings/Advanced/Advanced.tsx
+++ src/components/Settings/Advanced/Advanced.tsx
@@ -1,102 +1,84 @@
-import { useCallback, useEffect, useState } from 'react';
-import 'swiper/css';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { useMemo, useState } from 'react';
 import { startMasterCalibration } from '../../../api/api';
 
-import { SettingsKey } from '@meticulous-home/espresso-api';
 import { useHandleGestures } from '../../../hooks/useHandleGestures';
 import { SettingsItem } from '../../../types';
 import { useSettings, useUpdateSettings } from '../../../hooks/useSettings';
-import { marqueeIfNeeded } from '../../shared/MarqueeValue';
 import { setBubbleDisplay } from '../../store/features/screens/screens-slice';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { IdleScreens } from './IdleScreenSetting';
 import { useDeviceInfo } from '../../../hooks/useDeviceOSStatus';
+import Styled, {
+  VIEWPORT_HEIGHT,
+  MARQUEE_MIN_TEXT_LENGTH
+} from '../../../styles/utils/mixins';
+import { calculateOptionPosition } from '../../../styles/utils/calculateOptionPosition';
+import { getSettingLabel } from '../../../utils/settingsLabels';
+
+const initialSettings: SettingsItem[] = [
+  {
+    key: 'usb_mode',
+    label: 'USB mode'
+  },
+  {
+    key: 'master_calibration',
+    label: 'ACAIA master calibration',
+    visible: true
+  },
+  {
+    key: 'save_debug_shot_data',
+    label: 'Save debug shot data',
+    visible: true
+  },
+  {
+    key: 'set_update_channel',
+    label: 'Update channel',
+    visible: true
+  },
+  {
+    key: 'idle_screen',
+    label: 'Select Idle Screen',
+    visible: true
+  },
+  {
+    key: 'back',
+    label: 'Back',
+    visible: true
+  }
+];
 
 export const AdvancedSettings = () => {
   const dispatch = useAppDispatch();
-  const { data: globalSettings } = useSettings();
+  const { data: globalSettings, isSuccess } = useSettings();
   const updateSettings = useUpdateSettings();
-  const [swiper, setSwiper] = useState(null);
   const [activeIndex, setActiveIndex] = useState(0);
   const bubbleDisplay = useAppSelector((state) => state.screen.bubbleDisplay);
   const { refetch: fetchDeviceStatus } = useDeviceInfo();
 
-  const settings: SettingsItem[] = [
-    {
-      key: 'usb_mode',
-      label: 'USB mode'
-    },
-    {
-      key: 'master_calibration',
-      label: 'ACAIA master calibration',
-      visible: true
-    },
-    {
-      key: 'save_debug_shot_data',
-      label: 'Save debug shot data',
-      visible: true
-    },
-    {
-      key: 'set_update_channel',
-      label: 'Update channel',
-      visible: true
-    },
-    {
-      key: 'set_idle_screen',
-      label: 'Select Idle Screen',
-      visible: true
-    },
-    {
-      key: 'back',
-      label: 'Back',
-      visible: true
-    }
-  ];
-
-  const showValue = useCallback(
-    (isActive: boolean, item: SettingsItem) => {
-      if (!item) return <></>;
-      let val = item.label.toUpperCase();
-      if (globalSettings) {
-        switch (item.key) {
-          case 'usb_mode':
-            val = `${val}: ${globalSettings.usb_mode}`;
-            break;
-          case 'set_update_channel':
-            val = `${val}: ${globalSettings.update_channel}`;
-            break;
-          case 'set_idle_screen': {
-            const idleKey = globalSettings.idle_screen;
-            const idle = IdleScreens.filter((item) => item.key === idleKey)[0];
-            const idleLabel =
-              idle?.shortLabel || idle?.label || idleKey.toUpperCase();
-            val = `${val}: ${idleLabel}`;
-            break;
-          }
-          default:
-            if (typeof globalSettings[item.key as SettingsKey] === 'boolean') {
-              val = globalSettings[item.key as SettingsKey]
-                ? val + ': ENABLED'
-                : val + ': DISABLED';
-            }
-        }
-
-        return marqueeIfNeeded({ enabled: isActive, val });
-      }
-    },
-    [globalSettings]
+  const updatedSettings = useMemo(
+    () =>
+      isSuccess
+        ? initialSettings.map((item) => {
+            const newLabel = getSettingLabel(item.key, globalSettings);
+            return newLabel
+              ? { ...item, label: `${item.label}: ${newLabel}` }
+              : item;
+          })
+        : initialSettings,
+    [globalSettings, isSuccess]
   );
+
   useHandleGestures(
     {
       left() {
         setActiveIndex((prev) => Math.max(prev - 1, 0));
       },
       right() {
-        setActiveIndex((prev) => Math.min(prev + 1, settings.length - 1));
+        setActiveIndex((prev) =>
+          Math.min(prev + 1, updatedSettings.length - 1)
+        );
       },
       pressDown() {
-        const activeItem = settings[activeIndex].key;
+        const activeItem = updatedSettings[activeIndex].key;
         switch (activeItem) {
           case 'device_info':
             fetchDeviceStatus();
@@ -115,7 +97,7 @@ export const AdvancedSettings = () => {
               setBubbleDisplay({ visible: true, component: 'updateChannel' })
             );
             break;
-          case 'set_idle_screen':
+          case 'idle_screen':
             dispatch(
               setBubbleDisplay({
                 visible: true,
@@ -147,44 +129,54 @@ export const AdvancedSettings = () => {
     !bubbleDisplay.visible
   );
 
-  useEffect(() => {
-    if (swiper) {
-      swiper.slideTo(activeIndex, 0, false);
-    }
-  }, [activeIndex, swiper]);
+  const optionPositionOutter = useMemo(
+    () =>
+      calculateOptionPosition({
+        activeOptionIdx: activeIndex,
+        settings: updatedSettings
+      }),
+    [activeIndex, updatedSettings]
+  );
+
+  const optionPositionInner = useMemo(
+    () =>
+      calculateOptionPosition({
+        activeOptionIdx: activeIndex,
+        adjustmentFn: (position) => position - VIEWPORT_HEIGHT / 2,
+        settings: updatedSettings
+      }),
+    [activeIndex, updatedSettings]
+  );
 
   return (
-    <div className="main-quick-settings">
-      <Swiper
-        onSwiper={setSwiper}
-        slidesPerView={8}
-        allowTouchMove={false}
-        direction="vertical"
-        spaceBetween={16}
-        autoHeight={false}
-        centeredSlides={true}
-        initialSlide={activeIndex}
-        style={{ paddingLeft: '29px', top: '-4px' }}
-      >
-        {settings.map((item, index: number) => {
-          const isActive = index === activeIndex;
-          return (
-            <SwiperSlide
-              key={index}
-              className={`settings-item ${isActive ? 'active-setting' : ''}`}
-            >
-              <div className="settings-entry text-container">
-                <span
-                  className="settings-text"
-                  style={{ wordBreak: 'break-word' }}
-                >
-                  {showValue(isActive, item)}
-                </span>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </div>
+    <Styled.SettingsContainer>
+      <Styled.Viewport>
+        <Styled.OptionsContainer $translateY={optionPositionOutter}>
+          {updatedSettings.map((option) => (
+            <Styled.Option key={option.key}>
+              <span>{option.label}</span>
+            </Styled.Option>
+          ))}
+        </Styled.OptionsContainer>
+        <Styled.ActiveIndicator>
+          <Styled.OptionsContainer
+            $translateY={optionPositionInner}
+            $isInner={true}
+          >
+            {updatedSettings.map((option, index) => (
+              <Styled.Option
+                key={option.key}
+                $isMarquee={
+                  activeIndex === index &&
+                  option.label.length > MARQUEE_MIN_TEXT_LENGTH
+                }
+              >
+                <span>{option.label}</span>
+              </Styled.Option>
+            ))}
+          </Styled.OptionsContainer>
+        </Styled.ActiveIndicator>
+      </Styled.Viewport>
+    </Styled.SettingsContainer>
   );
 };
--- src/utils/settingsLabels.ts
+++ src/utils/settingsLabels.ts
@@ -1,4 +1,5 @@
 import { Settings } from '@meticulous-home/espresso-api';
+import { IdleScreens } from '../components/Settings/Advanced/IdleScreenSetting';
 
 export const getSettingLabel = (key: string, globalSettings: Settings) => {
   const settingLabelMap: Record<string, string | undefined> = {
@@ -10,7 +11,15 @@ export const getSettingLabel = (key: string, globalSettings: Settings) => {
     enable_sounds: globalSettings.enable_sounds ? 'ENABLED' : 'DISABLED',
     time_zone: globalSettings.time_zone ?? '',
     timezone_sync:
-      globalSettings.timezone_sync === 'automatic' ? 'ENABLED' : 'DISABLED'
+      globalSettings.timezone_sync === 'automatic' ? 'ENABLED' : 'DISABLED',
+    usb_mode: globalSettings.usb_mode,
+    set_update_channel: globalSettings.update_channel,
+    idle_screen: IdleScreens.find(
+      (item) => item.key === globalSettings.idle_screen
+    )?.shortLabel,
+    save_debug_shot_data: globalSettings.save_debug_shot_data
+      ? 'ENABLED'
+      : 'DISABLED'
   };
 
   return settingLabelMap[key];
