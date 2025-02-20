fa56ed7f397d36f4f5bf9b12c11555fba2883c49
Arturo Escobar <s.arturoec@gmail.com>
2025-02-20 12:17:19 -0600

    fix(bubble): fix exit animation for the bubble component

--- src/components/Bubble/Bubble.tsx
+++ src/components/Bubble/Bubble.tsx
@@ -1,43 +1,82 @@
+import { useEffect, useState, useRef } from 'react';
 import { useHandleGestures } from '../../../src/hooks/useHandleGestures';
 import { memoizedRoutes } from '../../../src/utils';
 import { setBubbleDisplay } from '../store/features/screens/screens-slice';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import './bubble.less';
 
-export const durationAnimation = 450;
-const animationStyle = { animationDuration: `${durationAnimation / 1000}s` };
-
 export default function Bubble() {
   const dispatch = useAppDispatch();
-  const Bubble = useAppSelector((state) => state.screen.bubbleDisplay);
   const bubbleDisplay = useAppSelector((state) => state.screen.bubbleDisplay);
-  const route = memoizedRoutes[Bubble.component];
+  const [animationState, setAnimationState] = useState({
+    isVisible: false,
+    isAnimating: false
+  });
+
+  const componentRef = useRef(null);
+
+  useEffect(() => {
+    if (bubbleDisplay.component) componentRef.current = bubbleDisplay.component;
+  }, [bubbleDisplay.component]);
+
+  useEffect(() => {
+    if (bubbleDisplay.visible)
+      setAnimationState({
+        isVisible: true,
+        isAnimating: false
+      });
+
+    if (!bubbleDisplay.visible && animationState.isVisible)
+      setAnimationState((prev) => ({ ...prev, isAnimating: true }));
+  }, [bubbleDisplay.visible]);
+
+  const route = componentRef.current
+    ? memoizedRoutes[componentRef.current]
+    : null;
 
   useHandleGestures({
     context() {
       dispatch(
         setBubbleDisplay({
           visible: !bubbleDisplay.visible,
-          component: !bubbleDisplay.visible ? 'quick-settings' : null
+          component: !bubbleDisplay.visible
+            ? 'quick-settings'
+            : bubbleDisplay.component
         })
       );
     }
   });
 
-  if (!Bubble || !Bubble.component) return <></>;
+  if (!animationState.isVisible && !animationState.isAnimating) return null;
+
+  const handleAnimationEnd = () => {
+    if (!animationState.isAnimating) return;
+
+    setAnimationState({
+      isVisible: false,
+      isAnimating: false
+    });
+
+    if (bubbleDisplay.component)
+      dispatch(
+        setBubbleDisplay({
+          visible: false,
+          component: null
+        })
+      );
+  };
+  console.log('Bubble', bubbleDisplay);
 
   return (
     <div
       className={`main-bubble main-layout ${
-        Bubble && Bubble.visible
+        !animationState.isAnimating
           ? 'bubble-enter-animation'
           : 'bubble-leave-animation'
       } large`}
-      style={animationStyle}
+      onAnimationEnd={handleAnimationEnd}
     >
-      <div className="bubble-container">
-        <route.component />
-      </div>
+      <div className="bubble-container">{route && <route.component />}</div>
     </div>
   );
 }
