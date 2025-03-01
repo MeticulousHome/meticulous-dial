d98f96dbb3326dbaaacb400bcec5d0a6a897e0ad
Mimoja <git@mimoja.de>
2025-03-01 03:01:57 +0100

    feat(profile-settings): rebuild settings screen with styled components
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/components/PressetSettings/PressetSettings.tsx
+++ src/components/PressetSettings/PressetSettings.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import 'swiper/css';
-import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { useHandleGestures } from '../../hooks/useHandleGestures';
-import { IPresetSetting } from '../../types';
 import { getPresetSettings } from '../../utils/preset';
 import {
   discardSettings,
@@ -13,26 +11,117 @@ import {
 } from '../store/features/preset/preset-slice';
 import { setScreen } from '../store/features/screens/screens-slice';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
-import { FormatSetting } from './FormatSetting';
-import './pressetSettings.css';
+
+import { css, keyframes, styled } from 'styled-components';
+import { api } from '../../api/api';
+import { IPresetNumericalUnit } from '../../types';
+
+const API_URL = window.env?.SERVER_URL || 'http://localhost:8080';
+
+const OPTIONS_HEIGHT = 50;
+const CARD_GAP = 2;
+const translationAnimationDuration = 150;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Viewport = styled.div<{
+  $translateY: number;
+}>`
+  height: 100%;
+  width: 75%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
+  gap: ${CARD_GAP}px;
+  transform: ${({ $translateY }) =>
+    `translateY(${$translateY + 240 - OPTIONS_HEIGHT / 2}px)`};
+  transition: transform ${translationAnimationDuration}ms ease;
+`;
+
+const MarqueeAnimation = keyframes` 
+  0% {
+    transform: translateX(0%);
+  }
+  50% {
+    transform: translateX(calc(120px - 100%));
+  }
+  100% {
+    transform: translateX(0%);
+  }
+`;
+
+const SettingsEntry = styled.div<{
+  $small?: boolean;
+  $active?: boolean;
+  $marquee?: boolean;
+}>`
+  display: flex;
+  flex-direction: row;
+  justify-content: left;
+  align-items: center;
+  height: ${OPTIONS_HEIGHT}px;
+  width: 100%;
+
+  font-family: 'ABC Diatype';
+  font-size: 40px;
+  font-weight: 500;
+  letter-spacing: -0.01em;
+  line-height: 1.2;
+  text-transform: capitalize;
+  white-space: nowrap;
+  ${(props) =>
+    props.$marquee &&
+    css`
+      animation: ${MarqueeAnimation} 8s ease-in-out infinite;
+    `};
+`;
+
+const SettingsLabel = styled.span<{ $active?: boolean }>`
+  padding-right: 8px;
+  color: ${(props) => (props.$active ? 'white' : '#e7e7e790')};
+`;
+
+const SettingsValue = styled.span<{ $small?: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  max-width: 400px;
+
+  font-size: ${(props) => (props.$small ? 25 : 40)}px;
+
+  color: #f5c444;
+  overflow: visible;
+`;
+
+const SettingsUnit = styled.span`
+  font-size: 17px;
+  font-family: 'ABC Diatype Mono';
+  letter-spacing: -0.5px;
+  text-transform: none;
+  color: #f5c444;
+  height: 100%;
+  margin-top: 10px;
+`;
 
 export function PressetSettings(): JSX.Element {
   const dispatch = useAppDispatch();
   const bubbleDisplay = useAppSelector((state) => state.screen.bubbleDisplay);
-  const [animationStyle, setAnimationStyle] = useState('');
-  const [swiper, setSwiper] = useState(null);
   const presets = useAppSelector((state) => state.presets);
-  const currentPresetSetting = useAppSelector(
-    (state) => state.presets.activePreset?.settings || []
-  );
 
   const settings = useMemo(() => {
     return getPresetSettings(presets);
   }, [presets.updatingSettings.settings]);
 
-  const [presetSettingIndex, setPresetSettingIndex] = useState<
-    IPresetSetting['key']
-  >(settings[presets.activeSetting].key);
+  const activeSetting = settings[presets.activeSetting];
 
   useHandleGestures(
     {
@@ -43,27 +132,27 @@ export function PressetSettings(): JSX.Element {
         dispatch(setNextSettingOption());
       },
       pressDown() {
-        if (presetSettingIndex === 'save') {
+        if (activeSetting.key === 'save') {
           dispatch(savePreset());
           dispatch(setScreen('pressets'));
-        } else if (presetSettingIndex == 'discard') {
+        } else if (activeSetting.key == 'discard') {
           dispatch(discardSettings());
           dispatch(setScreen('pressets'));
-        } else if (presetSettingIndex === 'name') {
+        } else if (activeSetting.key === 'name') {
           dispatch(setScreen('name'));
-        } else if (presetSettingIndex === 'output') {
+        } else if (activeSetting.key === 'output') {
           dispatch(setScreen('output'));
-        } else if (presetSettingIndex.includes('pressure')) {
+        } else if (activeSetting.key.includes('pressure')) {
           dispatch(setScreen('pressure'));
-        } else if (presetSettingIndex.includes('time')) {
+        } else if (activeSetting.key.includes('time')) {
           dispatch(setScreen('time'));
-        } else if (presetSettingIndex.includes('weight')) {
+        } else if (activeSetting.key.includes('weight')) {
           dispatch(setScreen('weight'));
-        } else if (presetSettingIndex.includes('flow')) {
+        } else if (activeSetting.key.includes('flow')) {
           dispatch(setScreen('flow'));
-        } else if (presetSettingIndex === 'temperature') {
+        } else if (activeSetting.key === 'temperature') {
           dispatch(setScreen('temperature'));
-        } else if (presetSettingIndex === 'image') {
+        } else if (activeSetting.key === 'image') {
           dispatch(setScreen('pressetProfileImage'));
         }
       }
@@ -71,89 +160,64 @@ export function PressetSettings(): JSX.Element {
     bubbleDisplay.visible
   );
 
-  useEffect(() => {
-    if (swiper) {
-      const settingsExist =
-        currentPresetSetting && currentPresetSetting.length > 0;
-
-      let settingBeforeChange;
-
-      if (settingsExist && currentPresetSetting[swiper['activeIndex']]) {
-        settingBeforeChange = currentPresetSetting[swiper['activeIndex']].key;
-      }
-
-      if (
-        (settingBeforeChange === 'save' || settingBeforeChange === 'discard') &&
-        presets.activeSetting === 2
-      ) {
-        swiper.slideTo(presets.activeSetting, 0, false);
-      } else {
-        swiper.slideTo(presets.activeSetting);
-      }
-
-      if (settingsExist) {
-        setPresetSettingIndex(settings[presets.activeSetting].key);
-      }
-    }
-  }, [presets.activeSetting, swiper]);
-
-  useEffect(() => {
-    if (
-      presets.activePreset &&
-      currentPresetSetting.length > 0 &&
-      currentPresetSetting[presets.activeSetting] &&
-      settings[presets.activeSetting]?.key
-    ) {
-      setPresetSettingIndex(settings[presets.activeSetting].key);
-    }
-  }, [presets.activePreset.settings]);
-
-  useEffect(() => {
-    return () => {
-      setAnimationStyle('');
-    };
-  }, []);
-
   return (
-    <div className="presset-container">
-      <div className="presset-options">
-        <Swiper
-          onSwiper={setSwiper}
-          slidesPerView={9}
-          allowTouchMove={false}
-          direction="vertical"
-          autoHeight={false}
-          centeredSlides={true}
-          initialSlide={presets.activeSetting}
-          onSlideNextTransitionStart={() => {
-            setAnimationStyle('animation-next');
-          }}
-          onSlidePrevTransitionStart={() => setAnimationStyle('animation-prev')}
-          onSlideChangeTransitionEnd={() => setAnimationStyle('')}
-        >
-          {settings.map((setting, index: number) => (
-            <SwiperSlide
-              className="presset-option-item"
-              key={`option-${index}`}
+    <Container>
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          background: 'linear-gradient(black 20%, transparent 50%, black 80%)',
+          pointerEvents: 'none',
+          zIndex: 1
+        }}
+      />
+      <Viewport
+        $translateY={-presets.activeSetting * (OPTIONS_HEIGHT + CARD_GAP)}
+      >
+        {settings.map((setting, index: number) => {
+          const { label, value, type } = setting;
+          const isActive = settings[presets.activeSetting].label === label;
+          const isImage = type === 'image';
+          const showValue =
+            isActive &&
+            (typeof value === 'number' || typeof value === 'string');
+          const characters =
+            label && value ? label.length + value.toString().length : 0;
+          return (
+            <SettingsEntry
+              $marquee={isActive && !isImage && characters > 24}
+              $active={isActive}
+              key={index}
             >
-              <div
-                className={`${animationStyle} ${
-                  setting.key === 'delete' ? 'delete-option-item' : ''
-                }`}
-              >
-                <FormatSetting
-                  setting={setting}
-                  isActive={
-                    settings[presets.activeSetting].label === setting.label
-                  }
-                />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <div className="fade fade-top"></div>
-      <div className="fade fade-bottom"></div>
-    </div>
+              <SettingsLabel $active={isActive}>
+                {label}
+                {showValue && ':'}
+              </SettingsLabel>
+              {isActive &&
+                (isImage ? (
+                  <img
+                    src={`${API_URL}${api.getProfileImageUrl(value)}`}
+                    alt="No image"
+                    width={OPTIONS_HEIGHT}
+                    height={OPTIONS_HEIGHT}
+                  />
+                ) : (
+                  <>
+                    <SettingsValue $small={characters > 21}>
+                      {showValue && (value || 0)}
+                    </SettingsValue>
+                    <SettingsUnit>
+                      {(setting as IPresetNumericalUnit)?.unit}
+                    </SettingsUnit>
+                  </>
+                ))}
+            </SettingsEntry>
+          );
+        })}
+      </Viewport>
+    </Container>
   );
 }
