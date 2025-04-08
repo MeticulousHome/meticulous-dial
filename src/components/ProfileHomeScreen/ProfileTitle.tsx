8cf976561eb748b974f2a26b7a1bbd3fed3ea87f
Mimoja <git@mimoja.de>
2025-04-08 14:48:46 +0200

    fix(profile-home): fix non-preset profile titles
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/components/ProfileHomeScreen/ProfileTitle.tsx
+++ src/components/ProfileHomeScreen/ProfileTitle.tsx
@@ -1,5 +1,6 @@
 import { useProfileContext } from '../../context/ProfileContext';
 import { styled } from 'styled-components';
+import { useAppSelector } from '../store/hooks';
 
 const PressetsTitle = styled.div<{ $scroll: boolean }>`
   height: 22px;
@@ -17,8 +18,17 @@ export const getActiveProfilesTitle = () => <TitleProfiles />;
 
 export const TitleProfiles = () => {
   const { localHoverState, localProfile } = useProfileContext();
+  const currentScreen = useAppSelector((state) => state.screen.value);
+  const forceTitle = currentScreen !== 'profileHome';
+
+  if (forceTitle) {
+    <PressetsTitle $scroll={false}>
+      <span>{localProfile?.name || ''}</span>
+    </PressetsTitle>;
+  }
+
   return (
-    <PressetsTitle $scroll={localHoverState}>
+    <PressetsTitle $scroll={localHoverState || forceTitle}>
       <span>Catalog</span>
       <span>{localProfile?.name || ''}</span>
     </PressetsTitle>
