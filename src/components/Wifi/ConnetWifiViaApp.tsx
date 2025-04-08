46950be28db5065d728cd6798728445612f495bc
Mimoja <git@mimoja.de>
2025-04-08 13:11:31 +0200

    fix(profile-home): fix wifi-qr code
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/components/Wifi/ConnetWifiViaApp.tsx
+++ src/components/Wifi/ConnetWifiViaApp.tsx
@@ -7,75 +7,48 @@ import { useAppDispatch } from '../store/hooks';
 import './connectWifiViaApp.css';
 import './wifiDetails.css';
 
-const items = [{ key: 'content' }, { key: 'back' }];
-
 export const ConnectWifiViaApp = (): JSX.Element => {
-  const [swiper, setSwiper] = useState(null);
-  const [activeIndex, setActiveIndex] = useState(0);
   const dispatch = useAppDispatch();
 
   useHandleGestures({
-    left() {
-      setActiveIndex((prev) => Math.max(prev - 1, 0));
-    },
-    right() {
-      setActiveIndex((prev) => Math.min(prev + 1, items.length - 1));
-    },
     pressDown() {
-      switch (items[activeIndex].key) {
-        case 'back':
-          dispatch(
-            setBubbleDisplay({ visible: true, component: 'connectWifiMenu' })
-          );
-          break;
-
-        default:
-          break;
-      }
+      dispatch(
+        setBubbleDisplay({ visible: true, component: 'connectWifiMenu' })
+      );
     }
   });
 
-  useEffect(() => {
-    if (swiper) {
-      swiper.slideTo(activeIndex, 0, false);
-    }
-  }, [activeIndex, swiper]);
-
   return (
-    <div className="main-quick-settings">
-      <Swiper
-        onSwiper={setSwiper}
-        slidesPerView={8}
-        allowTouchMove={false}
-        direction="vertical"
-        spaceBetween={16}
-        autoHeight={false}
-        initialSlide={activeIndex}
-        centeredSlides={true}
-        style={{ paddingLeft: '29px', top: '-4px' }}
-      >
-        <SwiperSlide key="content">
+    <div className="main-quick-settings settings-explanation-container">
+      <div className="settings-explanation">
+        <div className="settings-explanation-shaper-left" />
+        <div className="settings-explanation-shaper-right" />
+        <div className="wifi-connect-container">
           <div className="wifi-help-text">
             To connect the machine to your existing WiFi download the meticulous
             App in the AppStore or PlayStore and select "Add Device" in the
             settings menu. An authentication will be requested if needed and the
             app will quide you through the process.
           </div>
-        </SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
 
-        <SwiperSlide
-          key="back"
-          className={`settings-item ${
-            items[activeIndex].key === 'back' ? 'active-setting' : ''
-          }`}
-        >
-          <div className="settings-entry">
-            <span>Back</span>
+          <div
+            className={`settings-item active-setting`}
+            style={{
+              marginTop: 80,
+              marginBottom: 80
+            }}
+          >
+            <div
+              className="settings-entry"
+              style={{
+                padding: '6px'
+              }}
+            >
+              <span>Back</span>
+            </div>
           </div>
-        </SwiperSlide>
-      </Swiper>
+        </div>
+      </div>
     </div>
   );
 };
