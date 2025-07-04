0f9157bde96b70eeed020b9d48adcebf98ca8be6
Mimoja <git@mimoja.de>
2025-07-04 19:46:22 +0200

    feat(title): add marquee to the title bubble
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/components/ProfileHomeScreen/ProfileTitle.tsx
+++ src/components/ProfileHomeScreen/ProfileTitle.tsx
@@ -1,16 +1,44 @@
 import { useProfileContext } from '../../context/ProfileContext';
-import { styled } from 'styled-components';
+import { css, keyframes, styled } from 'styled-components';
 import { useAppSelector } from '../store/hooks';
+import { useEffect, useRef, useState } from 'react';
 
-const PressetsTitle = styled.div<{ $scroll: boolean }>`
-  height: 22px;
+const Title = styled.div<{ $scroll: boolean; $isMarquee?: boolean }>`
+  height: 44px;
   display: flex;
   flex-direction: column;
-  padding-top: 1.5px;
-
+  padding-top: 2px;
+  width: 100%;
   transition: transform 0.3s ease-in-out;
   transform: ${({ $scroll }) =>
-    $scroll ? 'translateY(-100%)' : 'translateY(0)'};
+    $scroll ? 'translateY(-50%)' : 'translateY(0)'};
+};
+`;
+
+const PresetTitleMarquee = (width: number) => keyframes`
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-${width}px);
+  }
+`;
+
+const TITLE_MAX_WIDTH = 400; // Maximum width for the title
+const TITLE_GAP = 40;
+
+const PresetTitle = styled.span<{ $marqueeWidth?: number }>`
+  width: fit-content;
+  min-width: 228px;
+  ${({ $marqueeWidth }) =>
+    $marqueeWidth > 0 &&
+    css`
+      position: relative;
+      display: inline-block;
+      animation: ${PresetTitleMarquee($marqueeWidth + TITLE_GAP)} 5s infinite
+        linear;
+      animation-delay: 0.9s;
+    `}
 `;
 
 export const getProfilesTitle = () => <TitleProfiles />;
@@ -19,18 +47,47 @@ export const getActiveProfilesTitle = () => <TitleProfiles />;
 export const TitleProfiles = () => {
   const { localHoverState, localProfile } = useProfileContext();
   const currentScreen = useAppSelector((state) => state.screen.value);
-  const forceTitle = currentScreen !== 'profileHome';
+  const isHomeScreen = currentScreen == 'profileHome';
+  const titleRef = useRef<HTMLSpanElement>(null);
 
-  if (forceTitle) {
-    <PressetsTitle $scroll={false}>
-      <span>{localProfile?.name || ''}</span>
-    </PressetsTitle>;
+  // Dynamically adjust marquee animation speed based on title width
+  const [marqueeWidth, setMarqueeWidth] = useState(0);
+
+  useEffect(() => {
+    if (titleRef.current) {
+      const width = titleRef.current.offsetWidth;
+      const parentWidth = titleRef.current.parentElement?.offsetWidth || 0;
+      console.log('Title width:', width, 'Parent width:', parentWidth);
+      setMarqueeWidth(
+        width > parentWidth ? Math.min(width, TITLE_MAX_WIDTH) : 0
+      );
+    }
+  }, [localProfile?.name, titleRef.current]);
+
+  const scroll = localHoverState && isHomeScreen;
+  const marquee = (isHomeScreen && scroll) || !isHomeScreen;
+
+  let title = localProfile?.name || '';
+  if (title.length > 40) {
+    title = title.slice(0, 40) + '...';
   }
 
   return (
-    <PressetsTitle $scroll={localHoverState || forceTitle}>
-      <span>Catalog</span>
-      <span>{localProfile?.name || ''}</span>
-    </PressetsTitle>
+    <Title $scroll={scroll} $isMarquee={false}>
+      {isHomeScreen && <span>Catalog</span>}
+      <div>
+        <PresetTitle ref={titleRef} $marqueeWidth={marquee ? marqueeWidth : 0}>
+          {title}
+        </PresetTitle>
+        {marquee && marqueeWidth > 0 && (
+          <PresetTitle
+            $marqueeWidth={marquee ? marqueeWidth : 0}
+            style={{ paddingLeft: TITLE_GAP }}
+          >
+            {title}
+          </PresetTitle>
+        )}
+      </div>
+    </Title>
   );
 };
