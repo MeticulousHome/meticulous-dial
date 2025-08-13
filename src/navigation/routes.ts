b5fb74e6dec3ccde8a67930ec5c0510112c7a56a
Angel-Padilla <angel.p.lopez@outlook.com>
2025-08-13 17:06:14 -0600

    chore(routes): rename shot volume gauge

--- src/navigation/routes.ts
+++ src/navigation/routes.ts
@@ -165,7 +165,7 @@ export const routes: Record<ScreenType, Route> = {
   retraction_volume: {
     component: RetractionSettingGauge,
     title: 'retraction',
-    bottomTitle: 'retraction volume',
+    bottomTitle: 'shot volume',
     props: {
       type: 'retraction_volume'
     },
