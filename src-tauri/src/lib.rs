4ecc79b335321491a64da3bea80b55a6624d3a15
Mimoja <git@mimoja.de>
2025-08-18 13:08:39 +0200

    feat(tauri): build using tauri with vite
    Signed-off-by: Mimoja <git@mimoja.de>

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,29 @@
+use tauri::Manager;
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+      .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            #[cfg(debug_assertions)]
+            {
+            window.open_devtools();
+            window.close_devtools();
+            }
+            #[cfg(not(debug_assertions))]
+            {
+            window.set_fullscreen(true).unwrap();
+            }
+            Ok(())
+        })
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
