23604527b5e5605ac1ed1596ca6e80ca3ae62b24
Mimoja <git@mimoja.de>
2025-08-19 13:36:13 +0200

    fix(api): remove trailing / in the server url
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,7 +8,7 @@ import Api, {
 } from '@meticulous-home/espresso-api';
 
 export const API_URL =
-  import.meta.env.VITE_SERVER_URL || 'http://localhost:8080/';
+  import.meta.env.VITE_SERVER_URL || 'http://localhost:8080';
 
 export const api = new Api(undefined, API_URL);
 
