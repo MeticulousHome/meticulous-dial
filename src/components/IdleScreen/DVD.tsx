f96cf142f0248f9012bfefd0984a7aa981a3a4e0
Angel-Padilla <angel.p.lopez@outlook.com>
2025-10-30 13:51:10 -0600

    chore(dvd): improve bouncing logic, reflect speed against against angle of incidence

--- src/components/IdleScreen/DVD.tsx
+++ src/components/IdleScreen/DVD.tsx
@@ -32,22 +32,23 @@ const Logo = styled.img`
   object-fit: contain;
 `;
 
-const ANIMATION_SPEED = 0.5;
+const ANIMATION_SPEED = 1;
+interface point2D {
+  x: number;
+  y: number;
+}
 
-const isPointOutsideCircle = (x: number, y: number) => {
+const isPointOutsideCircle = (point: point2D) => {
   const xCenter = 240;
   const yCenter = 240;
   const radius = 240;
   const distance = Math.sqrt(
-    Math.pow(x - xCenter, 2) + Math.pow(y - yCenter, 2)
+    Math.pow(point.x - xCenter, 2) + Math.pow(point.y - yCenter, 2)
   );
   return distance > radius;
 };
 
-function doOverlap(
-  position1: { x: number; y: number },
-  position2: { x: number; y: number }
-) {
+function doOverlap(position1: point2D, position2: point2D) {
   if (
     position1.x > position2.x + LogoSize ||
     position2.x > position1.x + LogoSize
@@ -113,38 +114,53 @@ const BouncyLogo: React.FC<{
   position: MutableRefObject<object>;
 }> = ({ imageSrc, position }) => {
   const logoRef = useRef<HTMLDivElement>(null);
-  const framesOutside = useRef(0);
+  const lastInboundPos = useRef<point2D>({ x: 0, y: 0 });
 
   useEffect(() => {
     let x = 100 + Math.random() * 100;
     let y = 100 + Math.random() * 100;
     position.current = { x, y };
-    let dirX = Math.random();
-    let dirY = Math.random();
+    let speed_angle = Math.random() * 2 * Math.PI;
+    let dirX = ANIMATION_SPEED * Math.cos(speed_angle);
+    let dirY = ANIMATION_SPEED * Math.sin(speed_angle);
 
     const elem = logoRef.current;
 
     const animate = () => {
       if (elem) {
-        if (
-          isPointOutsideCircle(x, y) ||
-          isPointOutsideCircle(x, y + LogoSize) ||
-          isPointOutsideCircle(x + LogoSize, y) ||
-          isPointOutsideCircle(x + LogoSize, y + LogoSize)
-        ) {
-          dirX = -dirX + (Math.random() * 2 - 1) * 0.3;
-          dirY = -dirY + (Math.random() * 2 - 1) * 0.3;
+        const logoCorners = [
+          { x, y },
+          { x: x, y: y + LogoSize },
+          { x: x + LogoSize, y: y },
+          { x: x + LogoSize, y: y + LogoSize }
+        ];
+        const offendingCorner = logoCorners.find(isPointOutsideCircle);
+        if (offendingCorner) {
+          x = lastInboundPos.current.x;
+          y = lastInboundPos.current.y;
+
+          // FOR A REASON UNBEKNOWNST TO ME, THIS SPAGHETTI WORKS BETTER THAN THE standard reflection formula IMPLMENTATION ( v' = v - 2 (v·n) n)
+          const reflection_axis: point2D = {
+            y: offendingCorner.y - 240,
+            x: offendingCorner.x - 240
+          };
+
+          const alpha =
+            Math.atan2(reflection_axis.y, reflection_axis.x) +
+            (reflection_axis.x > 0 ? 0 : Math.PI); //angle from circle center to the offendign corner
+          const beta =
+            Math.atan2(dirY, dirX) + (reflection_axis.x > 0 ? 0 : Math.PI); // angle of the speed vector
+          const d_angle = beta - alpha;
+          const tau = beta + d_angle; // calculate new speed vector (its inverted)
+          const reflected_angle = tau + Math.PI; // invert vector
+          const exit_angle =
+            reflected_angle + Math.random() * (d_angle < 0 ? -0.087 : 0.087); // add a random offset [-5deg, 5deg] prefering the direction of the reflected angle
+          dirX = Math.cos(exit_angle);
+          dirY = Math.sin(exit_angle);
+
           elem.style.backgroundColor = getNewRandomColor();
         } else {
-          framesOutside.current = 0;
-        }
-        framesOutside.current++;
-
-        if (framesOutside.current > 100) {
-          x = 100 + Math.random() * 100;
-          y = 100 + Math.random() * 100;
-          dirX = Math.random();
-          dirY = Math.random();
+          lastInboundPos.current = { x, y };
         }
 
         x += dirX * ANIMATION_SPEED;
