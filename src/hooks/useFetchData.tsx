05bbab3a21519ff5ab89de756c080aba8d708924
Mimoja <git@mimoja.de>
2025-11-22 15:33:16 +0100

    feat(ready): prefetch profile images and default profiles
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,7 +1,12 @@
 import { useEffect } from 'react';
 import { useAppDispatch } from '../components/store/hooks';
 import { loadNotifications } from '../components/store/features/notifications/notification-slice';
-import { useProfiles } from './useProfiles';
+import {
+  useDefaultProfiles,
+  useProfileDefaultImages,
+  useProfiles
+} from './useProfiles';
+import { api, API_URL } from '../api/api';
 
 export function useFetchData(onReady?: () => void) {
   const dispatch = useAppDispatch();
@@ -10,6 +15,37 @@ export function useFetchData(onReady?: () => void) {
     isError: profilesError,
     refetch: profilesRefetch
   } = useProfiles();
+
+  const { data: defaultProfiles } = useDefaultProfiles();
+  const { data: defaultImages } = useProfileDefaultImages();
+
+  useEffect(() => {
+    if (!defaultImages || defaultImages.length == 0) {
+      return;
+    }
+    defaultImages.forEach((picture) => {
+      const img = new Image();
+      img.src = picture;
+    });
+  }, [defaultImages]);
+
+  useEffect(() => {
+    if (!defaultProfiles) {
+      return;
+    }
+    defaultProfiles.community.forEach((profile) => {
+      const img = new Image();
+      img.src =
+        profile.display?.image &&
+        `${API_URL}${api.getProfileImageUrl(profile.display.image)}`;
+    });
+    defaultProfiles.default.forEach((profile) => {
+      const img = new Image();
+      img.src =
+        profile.display?.image &&
+        `${API_URL}${api.getProfileImageUrl(profile.display.image)}`;
+    });
+  }, [defaultImages]);
 
   useEffect(() => {
     dispatch(loadNotifications());
@@ -26,6 +62,12 @@ export function useFetchData(onReady?: () => void) {
 
   useEffect(() => {
     if (profiles && !profilesError) {
+      profiles.forEach((profile) => {
+        const img = new Image();
+        img.src =
+          profile.display?.image &&
+          `${API_URL}${api.getProfileImageUrl(profile.display.image)}`;
+      });
       console.log('calling onReady');
       if (onReady) {
         onReady();
