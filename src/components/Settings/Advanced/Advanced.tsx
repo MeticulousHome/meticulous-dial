60a7bec2e750a4156178f2aab4e244e276b03a09
Angel-Padilla <angel.p.lopez@outlook.com>
2026-02-13 13:59:23 -0600

    fix(advanced): fix 'test speaker' option label and prevent spamming test
    If the test is playing, do not allow to re test it until it finishes

--- src/components/Settings/Advanced/Advanced.tsx
+++ src/components/Settings/Advanced/Advanced.tsx
@@ -93,9 +93,7 @@ export const AdvancedSettings = () => {
     }
     const speakerTestSetting: SettingsItem = {
       key: 'speakerTest',
-      label: 'Test Speaker',
-      getLabel: (settings) =>
-        settings ? (testingSpeaker ? 'Testing' : 'Test') : 'Test',
+      label: `Test Speaker: ${testingSpeaker ? 'Testing' : 'Test'}`,
       visible: true
     };
 
@@ -216,11 +214,13 @@ export const AdvancedSettings = () => {
             dispatch(setScreen('displayAlignment'));
             break;
           case 'speakerTest':
-            setTestingSpeaker(true);
-            api.playSound(SPEAKER_TEST_SOUND).then(
-              () => console.log('testing speaker'),
-              (reason) => console.warn(`cannot test speaker: ${reason}`)
-            );
+            if (!testingSpeaker) {
+              setTestingSpeaker(true);
+              api.playSound(SPEAKER_TEST_SOUND).then(
+                () => console.log('testing speaker'),
+                (reason) => console.warn(`cannot test speaker: ${reason}`)
+              );
+            }
             break;
           case 'back':
             dispatch(
